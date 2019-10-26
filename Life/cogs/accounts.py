124512a3549935294e2e08ad741c6a3fefccc61e
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-10-26 02:48:38 +0100

    Minor fixes

--- Life/cogs/accounts.py
+++ Life/cogs/accounts.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-import discord
 
 
 class Accounts(commands.Cog):
@@ -23,32 +22,6 @@ class Accounts(commands.Cog):
         Delete your account.
         """
         return await self.bot.account_manager.delete_account(ctx, ctx.author.id)
-
-    @commands.command(name="inventory", aliases=["inv"])
-    async def inventory(self, ctx):
-
-        # Get the users account.
-        account = self.bot.account_manager.get_account(ctx.author.id)
-
-        # If no account was found return.
-        if not account:
-            return await ctx.send(f"You don't have an account. Use `{ctx.prefix}create` to make one.")
-
-        if not account.inventory:
-            return await ctx.send("You don't have any items.")
-
-        # Create an embed.
-        embed = discord.Embed(
-            title=f"{ctx.author}'s Inventory:",
-            description=f"",
-            colour=discord.Color.gold()
-        )
-        embed.set_footer(text=f"ID: {account.id}")
-        for item in account.inventory:
-            embed.description += f"{item.name} - Id: {item.id}\n"
-
-        # Send the embed.
-        return await ctx.send(embed=embed)
 
 
 def setup(bot):
--- Life/cogs/events.py
+++ Life/cogs/events.py
@@ -17,27 +17,29 @@ class Events(commands.Cog):
 
         print(f"\n[BOT] Logged in as {self.bot.user} - {self.bot.user.id}")
 
-        # Cache the current accounts in the database
+        # Fetch players from the database, and cache them.
         await self.bot.account_manager.cache_all_accounts()
 
-        # Set our user/guild blacklists.
+        # Fetch user/guild blacklists.
         blacklisted_users = await self.bot.db.fetch("SELECT * FROM user_blacklist")
         blacklisted_guilds = await self.bot.db.fetch("SELECT * FROM guild_blacklist")
 
-        # Append list of blacklisted guilds and users to respecitive lists.
+        # Append blacklisted users and guilds to the respective blacklists.
         for user in range(len(blacklisted_users)):
             self.bot.user_blacklist.append(int(blacklisted_users[user]["id"]))
         for user in range(len(blacklisted_guilds)):
             self.bot.guild_blacklist.append(int(blacklisted_guilds[user]["id"]))
 
-        # Leave any guilds that are blacklisted.
+        # Loop through the bots guilds.
         for guild in self.bot.guilds:
-            # If the guild is not in the blacklist, skip it.
-            if guild.id not in self.bot.guild_blacklist:
+            # Check if the guild is in the blacklist.
+            if guild.id in self.bot.guild_blacklist:
+                # The guild was in the blacklist, so leave it.
+                print(f"[BOT] Left blacklisted guild - {guild.id}")
+                await guild.leave()
+            else:
+                # The guild wasn't in the blacklist so skip it.
                 continue
-            # The guild was in the blacklist, so leave it.
-            print(f"[BOT] Left blacklisted guild - {guild.id}")
-            await guild.leave()
 
     @commands.Cog.listener()
     async def on_resume(self):
@@ -47,109 +49,119 @@ class Events(commands.Cog):
     async def on_disconnect(self):
         print(f"\n[BOT] Disconnected.")
 
-    # noinspection PyUnusedLocal
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        # If the edited message is not embedded or pinned, process it, this allows for uses to edit commands.
-        if not after.embeds and not after.pinned:
+
+        # If the edited message is not embedded or pinned, process it, this allows for users to edit commands.
+        if not after.embeds and not after.pinned and not before.pinned and not before.embeds:
 
             # Get the context of the message.
             ctx = await self.bot.get_context(after)
 
-            # Handle blacklisted users.
+            # If the message was a command.
             if ctx.command:
+                # And the author is in the user blacklist, dont process the command.
                 if after.author.id in self.bot.user_blacklist:
                     return await after.channel.send(f"Sorry, you are blacklisted.")
+                # Otherwise, process the message.
                 await self.bot.process_commands(after)
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
 
-        # Handle blacklisted users.
+        # If the author is blacklisted, dont let them use this command.
         if ctx.author.id in self.bot.user_blacklist:
             return await ctx.channel.send(f"Sorry, you are blacklisted.")
 
-        # Track which commands are used and in which guilds.
+        # Get the commands full name.
         parent = ctx.command.full_parent_name
         if parent:
             command = f"{parent} {ctx.command.name}"
         else:
             command = f"{ctx.command.name}"
 
-        # If the guild id is not already in the stats dict, add it.
+        # If the guild id is not already in the bots usage dict, add it.
         if ctx.guild.id not in self.bot.stats:
             self.bot.stats[ctx.guild.id] = {}
-        # If the command is not aleady in the guilds nested dict, add it
+        # If the command is not aleady in the guilds command usage, add it
         if command not in self.bot.stats[ctx.guild.id]:
             self.bot.stats[ctx.guild.id][command] = 1
-        # Else increment the command by 1.
+        # Otherwise increment the command usage by 1.
         else:
             self.bot.stats[ctx.guild.id][command] += 1
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+
         # If the command has a local error handler, return
         if hasattr(ctx.command, "on_error"):
             return
 
-        # Get the original exception or if nothing is found keep the exception.
+        # Get the original exception or if nothing is found, keep the original exception.
         error = getattr(error, "original", error)
 
         # Check for errors.
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"You missed the `{error.param}` parameter. You can use `{ctx.prefix}help {ctx.command}` for more information on what parameters to pass.")
-        if isinstance(error, commands.TooManyArguments):
-            await ctx.send(f"You passed too many arguments to the command `{ctx.command}`. You can use `{ctx.prefix}help {ctx.command}` for more information on what arguments to pass.")
-        if isinstance(error, commands.BadArgument):
-            return await ctx.send(f"You passed a bad arguement to the command `{ctx.command}`.")
+        message = ""
         if isinstance(error, commands.CommandNotFound):
             return
+        if isinstance(error, commands.MissingRequiredArgument):
+            message = f"You missed the `{error.param}` parameter. You can use `{ctx.prefix}help {ctx.command}` for more information on what parameters to pass."
+        if isinstance(error, commands.TooManyArguments):
+            message = f"You passed too many arguments to the command `{ctx.command}`. You can use `{ctx.prefix}help {ctx.command}` for more information on what arguments to pass."
+        if isinstance(error, commands.BadArgument):
+            message = f"You passed a bad arguement to the command `{ctx.command}`."
         if isinstance(error, commands.PrivateMessageOnly):
-            return await ctx.send(f"The command `{ctx.command}` can only be used in DM's.")
+            message = f"The command `{ctx.command}` can only be used in DM's."
         if isinstance(error, commands.NoPrivateMessage):
             try:
-                return await ctx.send(f"The command `{ctx.command}` can not be used in DM's.")
+                message = f"The command `{ctx.command}` can not be used in DM's."
             except discord.Forbidden:
                 return
         if isinstance(error, commands.NotOwner):
-            return await ctx.send(f"The command `{ctx.command}` is owner only.")
+            message = f"The command `{ctx.command}` is owner only."
         if isinstance(error, commands.MissingPermissions):
             missing_perms = ""
             for perm in error.missing_perms:
                 missing_perms += f"\n> {perm}"
-            return await ctx.send(f"You don't have the following permissions required to run the command `{ctx.command}`.\n{missing_perms}")
+            message = f"You don't have the following permissions required to run the command `{ctx.command}`.\n{missing_perms}"
         if isinstance(error, commands.BotMissingPermissions):
             missing_perms = ""
             for perm in error.missing_perms:
                 missing_perms += f"\n> {perm}"
-            return await ctx.send(f"I am missing the following permissions to run the command `{ctx.command}`.\n{missing_perms}")
+            message = f"I am missing the following permissions to run the command `{ctx.command}`.\n{missing_perms}"
         if isinstance(error, commands.DisabledCommand):
-            return await ctx.send(f"The command `{ctx.command}` is currently disabled.")
+            message = f"The command `{ctx.command}` is currently disabled."
         if isinstance(error, commands.CommandOnCooldown):
             if error.cooldown.type == commands.BucketType.user:
-                return await ctx.send(f"The command `{ctx.command}` is on cooldown for you, retry in `{formatting.get_time_friendly(error.retry_after)}`.")
+                message = f"The command `{ctx.command}` is on cooldown for you, retry in `{formatting.get_time_friendly(error.retry_after)}`."
             if error.cooldown.type == commands.BucketType.default:
-                return await ctx.send(f"The command `{ctx.command}` is on cooldown for the whole bot, retry in `{formatting.get_time_friendly(error.retry_after)}`.")
+                message = f"The command `{ctx.command}` is on cooldown for the whole bot, retry in `{formatting.get_time_friendly(error.retry_after)}`."
             if error.cooldown.type == commands.BucketType.guild:
-                return await ctx.send(f"The command `{ctx.command}` is on cooldown for this guild, retry in `{formatting.get_time_friendly(error.retry_after)}`.")
+                message = f"The command `{ctx.command}` is on cooldown for this guild, retry in `{formatting.get_time_friendly(error.retry_after)}`."
         if isinstance(error, exceptions.WrongGuild):
-            return await ctx.send("This command can't be used in this guild.")
+            message = "This command can't be used in this guild."
 
-        # Print the error and traceback if it doesnt match any of the above.
+        # If an error was caught, send it.
+        if message:
+            return await ctx.send(message)
+        # Otherwise, print the original error as a traceback.
         print(f"Ignoring exception in command {ctx.command}:")
         traceback.print_exception(type(error), error, error.__traceback__)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
+
         # Log the guild that was joined.
         print(f"\n[BOT] Joined a guild - {guild.name}")
 
+        # If the guild is the blacklist, leave it.
         if guild.id in self.bot.guild_blacklist:
             print(f"[BOT] Left blacklisted guild - {guild.name}")
             return await guild.leave()
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
+
         # Log the guild that was left.
         print(f"\n[BOT] Left a guild - {guild.name}")
 
