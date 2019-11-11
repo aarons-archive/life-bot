19354009a2af6f2375a361aa2aa09536196be141
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-11-11 00:43:56 +0000

    Redid bot help and cog help

--- Life/cogs/help.py
+++ Life/cogs/help.py
@@ -32,111 +32,74 @@ class HelpCommand(commands.HelpCommand):
             await ctx.send(str(error.original))
 
     async def send_bot_help(self, mapping):
-        # Get the ctx and owner of bot.
+
+        # Get the current context.
         ctx = self.context
-        owner = ctx.bot.get_user(238356301439041536)
+
         # Define an embed.
         embed = discord.Embed(
             colour=discord.Color.gold(),
-            title="__Life's help page__",
-            description=f"Use `{ctx.prefix}help [command]` for more info on a command.\n"
-                        f"You can also use `{ctx.prefix}help [category]` for more info on a category.\n"
+            title=f"__{ctx.bot.user.name}'s help page__",
+            description=f"Use `{ctx.prefix}help [command/Category]` for more info on a command or category.\n"
         )
+
         # Loop through all the cogs.
         for cog in ctx.bot.cogs.values():
-            # If this cogs has no commands or the commands are hidden, goto the next cog.
-            if sum(1 for c in cog.get_commands() if not (ctx.author != owner and c.hidden)) == 0:
+            # If the author is a bot owner, fetch all commands.
+            if ctx.author.id in ctx.bot.owner_ids:
+                cog_commands = [command for command in cog.get_commands()]
+            # Else, just fetch non hidden commands.
+            else:
+                cog_commands = [command for command in cog.get_commands() if command.hidden == False]
+            # If no commands where found, skip this cog.
+            if len(cog_commands) == 0:
                 continue
-            # Add cog name to description.
+            # Add this cogs name to the embed description.
             embed.description += f"\n__**{cog.qualified_name}:**__\n"
             # Loop through the commands in this cog.
-            for command in cog.get_commands():
-                # If the author is not the owner.
-                if not ctx.author.id == owner.id:
-                    # If the command is hidden, skip this command.
-                    if command.hidden is True:
-                        continue
-                    # Add the command to the discription.
-                    embed.description += f"{command.name} \u200b "
-                # Else, add the command to the discription even if it is hidden.
-                else:
-                    embed.description += f"{command.name} \u200b "
+            for command in cog_commands:
+                # Add the command to the embeds discription.
+                embed.description += f"`{command.name}` \u200b "
         # Send the embed.
         return await ctx.send(embed=embed)
 
     async def send_cog_help(self, cog):
-        # Get the ctx and owner of bot.
+        # Get the current context.
         ctx = self.context
-        owner = ctx.bot.get_user(238356301439041536)
-        # If this cogs has no commands or the commands are hidden, don"t display the help command.
-        if sum(1 for c in cog.get_commands() if not (c.hidden and ctx.author != owner)) == 0:
-            return await ctx.send("That extension is hidden/is owner only.")
+
         # Define an embed.
         embed = discord.Embed(
             colour=discord.Color.gold(),
-            title=f"__**{cog.qualified_name} cog help page.**__",
-            description=f""
+            title=f"__{cog.qualified_name} cog help page__",
+            description=f"Use `{ctx.prefix}help [command]` for more info on a command.\n\n"
         )
-        embed.description += f"Use `{ctx.prefix}help [command]` for more info on a command.\n\n"
-        # Loop through all the commands in this cog.
-        for command in cog.get_commands():
-            # Get the command name along with all its aliases.
-            command_name = f"{self.get_full_command(command)}"
-            # If the author is not the owner.
-            if not ctx.author.id == owner.id:
-                # If the command is hidden, skip this command.
-                if command.hidden is True:
-                    continue
-                # If the command has a help string.
-                if command.help:
-                    # Get the first line of the help string.
-                    command_help = f" - " + command.help.strip().split("\n")[0]
-                else:
-                    # The command did not have a help string.
-                    command_help = f" - No help provided for this command."
-                # Add command to the embed.
-                embed.description += f"**{command_name}**{command_help}\n"
-            # Else, add the command to the discription even if it is hidden.
+
+        # If the author is a bot owner, fetch all commands.
+        if ctx.author.id in ctx.bot.owner_ids:
+            cog_commands = [command for command in cog.get_commands()]
+        # Else, just fetch non hidden commands.
+        else:
+            cog_commands = [command for command in cog.get_commands() if command.hidden == False]
+
+        # If no commands where found, return an error.
+        if len(cog_commands) == 0:
+            return await ctx.send("This cog has no commands. This could be because they are hidden, or there are just no commands.")
+
+        for command in cog_commands:
+            command_name = self.get_full_command(command)
+            if command.help:
+                command_help = command.help.strip().split("\n")[0]
             else:
-                # If the command has a help string.
-                if command.help:
-                    # Get the first line of the help string.
-                    command_help = f" - " + command.help.strip().split("\n")[0]
-                else:
-                    # The command did not have a help string.
-                    command_help = f" - No help provided for this command."
-                # Add command to the embed.
-                embed.description += f"**{command_name}**{command_help}\n"
-            # If the command is a group command.
+                command_help = "No help provided for this command."
+            embed.description += f"**{command_name}** - {command_help}\n"
             if isinstance(command, commands.Group):
-                # Loop through the command in the group.
                 for group_command in command.commands:
-                    group_command_name = f"{self.get_full_command(group_command)}"
-                    # If the author is not the owner.
-                    if not ctx.author.id == owner.id:
-                        # If the command is hidden, skip this command.
-                        if group_command.hidden is True:
-                            continue
-                        # If the command has a help string.
-                        if group_command.help:
-                            # Get the first line of the help string.
-                            group_command_help = f" - " + group_command.help.strip().split("\n")[0]
-                        else:
-                            # The command did not have a help string.
-                            group_command_help = f" - No help provided for this command."
-                        # Add command to the embed.
-                        embed.description += f"**{group_command_name}**{group_command_help}\n"
-                    # Else, add the command to the discription even if it is hidden.
+                    group_command_name = self.get_full_command(group_command)
+                    if group_command.help:
+                        group_command_help = group_command.help.strip().split("\n")[0]
                     else:
-                        # If the command has a help string.
-                        if group_command.help:
-                            # Get the first line of the help string.
-                            group_command_help = f" - " + group_command.help.strip().split("\n")[0]
-                        else:
-                            # The command did not have a help string.
-                            group_command_help = f" - No help provided for this command."
-                        # Add command to the embed.
-                        embed.description += f"**{group_command_name}**{group_command_help}\n"
+                        group_command_help = "No help provided for this command."
+                    embed.description += f"`⮡\u200b` **{group_command_name}** - {group_command_help}\n"
         return await ctx.send(embed=embed)
 
     async def send_command_help(self, command):
