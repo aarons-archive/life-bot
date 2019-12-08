77345c6c375ee7d709bc292fe44d89b2332e3efa
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-12-08 22:58:48 +0000

    queue command correct upcoming songs

--- Life/cogs/music/music.py
+++ Life/cogs/music/music.py
@@ -285,7 +285,7 @@ class Music(commands.Cog):
         title = f"__**Current track:**__\n[{ctx.player.current.title}]({ctx.player.current.uri}) | " \
                   f"`{utils.format_time(round(ctx.player.current.length) / 1000)}` | " \
                   f"`Requested by:` {ctx.player.current.requester.mention}\n\n" \
-                  f"__**Up next:**__: Showing `10` out of `{ctx.player.queue.size()}` entries in the queue.\n"
+                  f"__**Up next:**__: Showing `{min([10, ctx.player.queue.size()])}` out of `{ctx.player.queue.size()}` entries in the queue.\n"
 
         entries = []
         for index, track in enumerate(ctx.player.queue.queue):
