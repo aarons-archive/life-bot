7dcb9562a8f57b1594fc7f94caf56031f980d1af
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-12-30 19:15:04 +0000

    get_item funcs now return a list

--- Life/cogs/rpg/objects/inventory.py
+++ Life/cogs/rpg/objects/inventory.py
@@ -2,7 +2,7 @@ from .boots import *
 from .chestplate import *
 from .helmet import *
 
-item_list = {
+item_ids = {
     1: StarterBoots,
     101: StarterChestplate,
     201: StarterHelmet
@@ -13,70 +13,72 @@ class Inventory:
 
     def __init__(self, raw_inventory: list):
 
-        self.raw = raw_inventory
-        self.items = self.get_inventory_objects(self.raw)
+        self.raw_items = raw_inventory
+        self.items = self.get_inventory_objects(self.raw_items)
 
     def get_inventory_objects(self, items: list):
 
         inventory = []
 
         for item in items:
-            item_object = item_list.get(item["id"], None)
+            item_object = item_ids.get(item["id"], None)
             if not item_object:
                 continue
             inventory.append(item_object(item))
 
         return inventory
 
-    def get_item_id(self, item_id: int):
+    def get_item_id(self, item_list: list, item_id: int):
 
-        items_of_id = [item for item in self.items if item.id == item_id]
+        items_of_id = [item for item in item_list if item.id == item_id]
 
         if not items_of_id:
             return None
 
-        elif len(items_of_id) == 1:
-            return items_of_id[0]
+        return items_of_id
 
-        else:
-            return items_of_id
+    def get_item_slot(self, item_list: list, item_slot: str):
 
-    def get_item_slot(self, item_slot: str):
-
-        items_of_slot = [item for item in self.items if item.slot == item_slot]
+        items_of_slot = [item for item in item_list if item.slot == item_slot]
 
         if not items_of_slot:
             return None
 
-        elif len(items_of_slot) == 1:
-            return items_of_slot[0]
+        return items_of_slot
 
-        else:
-            return items_of_slot
+    def get_item_base_name(self, item_list: list, item_base_name: str):
 
-    def get_item_base_type(self, item_base_type: str):
+        items_of_base_name = [item for item in item_list if item.base_name == item_base_name]
 
-        items_of_base_type = [item for item in self.items if item.base_type == item_base_type]
+        if not items_of_base_name:
+            return None
+
+        return items_of_base_name
+
+    def get_item_name(self, item_list: list, item_name: str):
+
+        items_of_name = [item for item in item_list if item.name == item_name]
+
+        if not items_of_name:
+            return None
+
+        return items_of_name
+
+    def get_item_base_type(self, item_list: list, item_base_type: str):
+
+        items_of_base_type = [item for item in item_list if item.base_type == item_base_type]
 
         if not items_of_base_type:
             return None
 
-        elif len(items_of_base_type) == 1:
-            return items_of_base_type[0]
+        return items_of_base_type
 
-        else:
-            return items_of_base_type
+    def get_item_type(self, item_list: list, item_type: str):
 
-    def get_item_type(self, item_type: str):
-
-        items_of_type = [item for item in self.items if item.type == item_type]
+        items_of_type = [item for item in item_list if item.type == item_type]
 
         if not items_of_type:
             return None
 
-        elif len(items_of_type) == 1:
-            return items_of_type[0]
-
-        else:
-            return items_of_type
+        return items_of_type
 
