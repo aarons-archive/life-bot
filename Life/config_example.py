1e2012822b630b1a969ece6905473953a2a75c64
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-03-21 22:47:05 +0000

    add new stuff to config example

--- Life/config_example.py
+++ Life/config_example.py
@@ -4,10 +4,22 @@ DISCORD_TOKEN = ""
 # Bot prefix
 DISCORD_PREFIX = "l-"
 
+# Osu Api Key
+OSU_TOKEN = ""
+
 # PSQL
 DB_CONN_INFO = {
     "user": "",
     "password": "",
     "host": "",
     "database": ""
+}
+
+NODES = {
+    "NODE_1": {
+        "host": "",
+        "port": "",
+        "password": "",
+        "identifier": ""
+    }
 }
