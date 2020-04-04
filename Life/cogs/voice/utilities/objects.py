4e44dd57046ed6cdb3ebcba3a55b420955c4b965
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-04-04 22:19:41 +0100

    added repr to SpotifyTrack and moved repr of Playlist

--- Life/cogs/voice/utilities/objects.py
+++ Life/cogs/voice/utilities/objects.py
@@ -26,10 +26,14 @@ class SpotifyTrack:
     def __init__(self, ctx: commands.Context, title: str, uri: str, length: int):
 
         self.title = title
-        self.ctx = ctx
         self.uri = uri
-        self.requester = ctx.author
         self.length = length
+
+        self.ctx = ctx
+        self.requester = ctx.author
+
+    def __repr__(self):
+        return f"<LifeSpotifyTrack title={self.title!r} uri=<{self.uri!r}> length={self.length}>"
 
 
 class Playlist:
@@ -46,11 +50,9 @@ class Playlist:
 
         self.tracks = []
 
+    def __repr__(self):
+        return f"<LifePlaylist id={self.id!r} name={self.name!r} owner_id={self.owner_id!r}>"
+
     @property
     def track_ids(self):
         return [track.track_id for track in self.tracks]
-
-    def __repr__(self):
-        return f"<LifePlaylist id={self.id} name={self.name} owner_id={self.owner_id}>"
-
-
