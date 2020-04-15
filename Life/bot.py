072342e0ec58d6141618f81f7d409461554a8db5
MyNameBeMrRandom <mynamebemrrandom@gmail.com>
2020-04-15 09:17:18 +0100

    use one big check instead of seperate ones

--- Life/bot.py
+++ Life/bot.py
@@ -5,14 +5,15 @@ import time
 
 import aiohttp
 import asyncpg
-import psutil
 import discord
+import psutil
 from discord.ext import commands
 
 import config
-from cogs.utilities import paginators
+from cogs.utilities import exceptions, paginators
 from cogs.utilities.utils import Utils
 from cogs.voice.utilities.player import Player
+
 
 os.environ["JISHAKU_HIDE"] = "True"
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
@@ -36,6 +37,9 @@ class MyContext(commands.Context):
 
     @property
     def player(self):
+        
+        if not self.bot.is_ready():
+            raise exceptions.BotNotReadyError
 
         return self.bot.granitepy.get_player(self.guild, cls=Player)
 
@@ -80,8 +84,7 @@ class Life(commands.AutoShardedBot):
         self.session = None
         self.db = None
 
-        self.bot.add_check(self.blacklist_check)
-        self.bot.add_check(self.permissions_check)
+        self.bot.add_check(self.check_can_run)
 
         self.general_perms = discord.Permissions(add_reactions=True, read_messages=True, send_messages=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
         self.voice_perms = discord.Permissions(permissions=self.general_perms.value, connect=True, speak=True)
@@ -140,22 +143,22 @@ class Life(commands.AutoShardedBot):
 
         await self.process_commands(message)
         
-    async def permissions_check(self, ctx):
+    async def check_can_run(self, ctx):
         
-        me = ctx.guild.me
+        if not ctx.guild:
+            raise commands.NoPrivateMessage()
         
+        if ctx.author.id in self.bot.user_blacklist.keys():
+            raise commands.CheckFailure(f"You are blacklisted from using this bot with reason `{self.bot.user_blacklist[ctx.author.id]}`")
+        
+        me = ctx.guild.me if ctx.guild else self.bot.user
         needed_perms = {perm: value for perm, value in dict(self.general_perms).items() if value is not False}
         current_perms = dict(me.permissions_in(ctx.channel))
         missing = [perm for perm, value in needed_perms.items() if current_perms[perm] != value]
         
         if missing:
             raise commands.BotMissingPermissions(missing)
-        return True
         
-    async def blacklist_check(self, ctx):
-
-        if ctx.author.id in self.bot.user_blacklist.keys():
-            raise commands.CheckFailure(f"You are blacklisted from using this bot with reason `{self.bot.user_blacklist[ctx.author.id]}`")
         return True
 
     async def bot_start(self):
