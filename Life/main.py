3e0980481a085c7484dd2ea771e35f9bc2644803
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-05-14 21:49:12 +0100

    rename log file to diorite

--- Life/main.py
+++ Life/main.py
@@ -6,7 +6,7 @@ from logging import handlers
 logger = logging.getLogger('diorite')
 logger.setLevel(logging.DEBUG)
 
-handler = logging.handlers.RotatingFileHandler(filename='logs/granitepy.log', mode='w', backupCount=10)
+handler = logging.handlers.RotatingFileHandler(filename='logs/diorite.log', mode='w', backupCount=10)
 handler.doRollover()
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt=f'%d/%m/%Y %I:%M:%S %p')
