6fda471fb6b6ea9ad7dfe605534f187a5a519bed
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-07-02 00:18:53 +0100

    broken command errors are now logged to a channel

--- Life/cogs/events.py
+++ Life/cogs/events.py
@@ -35,6 +35,7 @@ class Events(commands.Cog):
         self.bot.log_channel = self.bot.get_channel(697324016658022441)
         self.bot.dm_channel = self.bot.get_channel(714307639609131018)
         self.bot.mention_channel = self.bot.get_channel(714601090771058719)
+        self.bot.error_channel = self.bot.get_channel(728000870230523906)
 
         for guild in self.bot.guilds:
             if guild.id in self.bot.guild_blacklist:
@@ -78,15 +79,15 @@ class Events(commands.Cog):
 
         if self.bot.user in message.mentions:
 
-            guild = f'**Guild:** {ctx.guild}\n' if ctx.guild else ''
-            guild_id = f'**Guild ID:** {ctx.guild.id}\n' if ctx.guild else ''
+            guild = f'`Guild:` {ctx.guild}\n' if ctx.guild else ''
+            guild_id = f'`Guild ID:` {ctx.guild.id}\n' if ctx.guild else ''
             channel = f'#{ctx.channel}' if isinstance(ctx.channel, discord.TextChannel) else ctx.channel
-            info = f'{guild}{guild_id}**Channel:** {channel}\n**Channel ID:** {ctx.channel.id}'
+            info = f'{guild}{guild_id}`Channel:` {channel}\n`Channel ID:` {ctx.channel.id}'
 
             embed = discord.Embed(colour=discord.Colour.gold())
             embed.set_author(name=f'Mentioned by {ctx.author}', icon_url=self.bot.utils.member_avatar(ctx.author))
             embed.description = f'{message.content}'
-            embed.add_field(name='**Info:**', value=info)
+            embed.add_field(name='Info:', value=info)
             await self.bot.mention_channel.send(embed=embed)
 
         if message.guild is None:
@@ -95,7 +96,7 @@ class Events(commands.Cog):
             embed.set_author(name=f'DM from {ctx.author}', icon_url=self.bot.utils.member_avatar(ctx.author))
 
             embed.description = f'{message.content}'
-            embed.add_field(name='**Info:**', value=f'**Channel:** {ctx.channel}\n**Channel ID:** {ctx.channel.id}')
+            embed.add_field(name='Info:', value=f'`Channel:` {ctx.channel}\n`Channel ID:` {ctx.channel.id}')
             await self.bot.dm_channel.send(embed=embed)
 
     @commands.Cog.listener()
@@ -110,11 +111,64 @@ class Events(commands.Cog):
         await self.bot.process_commands(after)
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
+    async def on_command_error(self, ctx: commands.Context, error):
 
-        error = getattr(error, 'original', error)
+        prefix = self.bot.config.prefix
         command = ctx.command
-        prefix = self.bot.config.PREFIX
+
+        if ctx.author.id in self.bot.owner_ids:
+            command.reset_cooldown(ctx)
+
+        if isinstance(error, commands.CommandNotFound):
+            return  # Ignore this exception
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            return await ctx.send(f'You missed the `{error.param.name}` parameter for the command `{command}`. '
+                                  f'Use `{self.bot.config.prefix}help {command}` for more information on what '
+                                  f'parameters to use.')
+
+        elif isinstance(error, commands.MissingPermissions):
+            permissions = '\n'.join([f'> {permission}' for permission in error.missing_perms])
+            return await ctx.send(f'You are missing the following permissions required to run the command '
+                                  f'`{command}`.\n{permissions}')
+
+        elif isinstance(error, commands.BotMissingPermissions):
+            permissions = '\n'.join([f'> {permission}' for permission in error.missing_perms])
+            message = f'I am missing the following permissions required to run the command `{command}`.' \
+                      f'\n{permissions}'
+            try:
+                return await ctx.send(message)
+            except discord.Forbidden:
+                try:
+                    return await ctx.author.send(message)
+                except discord.Forbidden:
+                    return
+
+        elif isinstance(error, commands.CommandOnCooldown):
+            cooldowns = {
+                commands.BucketType.default: f'for the whole bot.',
+                commands.BucketType.user: f'for you.',
+                commands.BucketType.guild: f'for this server.',
+                commands.BucketType.channel: f'for this channel.',
+                commands.BucketType.member: f'cooldown for you.',
+                commands.BucketType.category: f'for this channel category.',
+                commands.BucketType.role: f'for your role.'
+            }
+            return await ctx.send(f'The command `{command}` is on cooldown {cooldowns[error.cooldown.type]}'
+                                  f'You can retry in `{self.bot.utils.format_time(error.retry_after, friendly=True)}`')
+
+        elif isinstance(error, commands.MaxConcurrencyReached):
+            cooldowns = {
+                commands.BucketType.default: f'.',
+                commands.BucketType.user: f' per user.',
+                commands.BucketType.guild: f' per server.',
+                commands.BucketType.channel: f' per channel.',
+                commands.BucketType.member: f' per member.',
+                commands.BucketType.category: f' per channel category.',
+                commands.BucketType.role: f' per role.'
+            }
+            return await ctx.send(f'The command `{command}` is already being ran at its maximum of '
+                                  f'{error.number} time(s){cooldowns[error.per]} Retry a bit later.')
 
         error_messages = {
             exceptions.BotNotReadyError: f'The bot is not ready yet.',
@@ -137,68 +191,30 @@ class Events(commands.Cog):
 
         error_message = error_messages.get(type(error))
 
-        if isinstance(error, commands.MissingRequiredArgument):
-            error_message = f'You missed the `{error.param.name}` parameter for the command `{command}`. ' \
-                            f'Use `{prefix}help {command}` for more information on what parameters to use.'
-
-        elif isinstance(error, commands.MissingPermissions):
-            permissions = '\n'.join([f'> {permission}' for permission in error.missing_perms])
-            error_message = f'You are missing the following permissions required to run the command ' \
-                            f'`{command}`.\n{permissions}'
-
-        elif isinstance(error, commands.BotMissingPermissions):
-            permissions = '\n'.join([f'> {permission}' for permission in error.missing_perms])
-            error_message = f'I am missing the following permissions required to run the command ' \
-                            f'`{command}`.\n{permissions}'
-
-        elif isinstance(error, commands.CommandOnCooldown):
-            cooldowns = {
-                commands.BucketType.default: f'The command `{command}` is on cooldown for the whole bot.',
-                commands.BucketType.user: f'The command `{command}` is on cooldown for you.',
-                commands.BucketType.guild: f'The command `{command}` is on cooldown for this server.',
-                commands.BucketType.channel: f'The command `{command}` is on cooldown for this channel.',
-                commands.BucketType.member: f'The command `{command}` is on cooldown for you.',
-                commands.BucketType.category: f'The command `{command}` is on cooldown for this channel category.',
-                commands.BucketType.role: f'The command `{command}` is on cooldown for your role.'
-            }
-            error_message = f'{cooldowns[error.cooldown.type]} You can retry in ' \
-                            f'`{self.bot.utils.format_time(error.retry_after, friendly=True)}`'
-
-        elif isinstance(error, commands.MaxConcurrencyReached):
-            cooldowns = {
-                commands.BucketType.default: f'.',
-                commands.BucketType.user: f' per user.',
-                commands.BucketType.guild: f' per guild.',
-                commands.BucketType.channel: f' per channel.',
-                commands.BucketType.member: f' per member.',
-                commands.BucketType.category: f' per channel category.',
-                commands.BucketType.role: f' per role.'
-            }
-            error_message = f'The command `{command}` is already being ran at its maximum of ' \
-                            f'{error.number} time(s){cooldowns[error.per]} Retry a bit later.'
-
-        elif isinstance(error, commands.CommandNotFound):
-            return
-
         if error_message:
             try:
                 return await ctx.send(error_message)
             except discord.Forbidden:
-                try:
-                    return await ctx.author.send(error_message)
-                except discord.Forbidden:
-                    return
+                pass
+
+        command.reset_cooldown(ctx)
+        error = error.original
+
+        guild = f'`Guild:` {ctx.guild}\n' if ctx.guild else ''
+        guild_id = f'`Guild ID:` {ctx.guild.id}\n' if ctx.guild else ''
+        channel = f'#{ctx.channel}' if isinstance(ctx.channel, discord.TextChannel) else ctx.channel
+        info = f'{guild}{guild_id}`Channel:` {channel}\n`Channel ID:` {ctx.channel.id}'
+        name = f'Error in command {command} used by {ctx.author}'
+
+        embed = discord.Embed(colour=discord.Colour.gold())
+        embed.set_author(name=name, icon_url=self.bot.utils.member_avatar(ctx.author))
+        embed.add_field(name='Info:', value=info)
+        error_traceback = f'```{"".join(traceback.format_exception(type(error), error, error.__traceback__))}```'
+        await self.bot.error_channel.send(error_traceback)
+        await self.bot.error_channel.send(embed=embed)
+        await ctx.send('Something went wrong while executing that command.')
 
         traceback.print_exception(type(error), error, error.__traceback__)
-
-    @commands.Cog.listener()
-    async def on_command(self, ctx):
-
-        self.bot.log.info(f'Command used. Command: {ctx.command.qualified_name} | '
-                          f'Guild: {ctx.guild} | Channel: {ctx.channel} | Author:  {ctx.author}')
-
-        if ctx.author.id in self.bot.owner_ids:
-            ctx.command.reset_cooldown(ctx)
 
     @commands.Cog.listener()
     async def on_socket_response(self, msg):
