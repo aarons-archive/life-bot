302e594f8ac1ea146c20584ebcc6ec58e2e84790
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-07-02 00:18:27 +0100

    update embeds to fit with new disocrd embed theme

--- Life/cogs/information.py
+++ Life/cogs/information.py
@@ -49,31 +49,30 @@ class Information(commands.Cog):
         Display the bots stats.
         """
 
-        latency_ms, average_latency_ms, typing_ms, discord_ms = await self.bot.utils.ping(ctx)
-        uptime = round(time.time() - self.bot.start_time)
+        uyviyvy
+        latency_ms, typing_ms, discord_ms = await self.bot.utils.ping(ctx)
+        uptime = self.bot.utils.format_time(seconds=round(time.time() - self.bot.start_time), friendly=True)
         files, functions, lines, classes = self.bot.utils.linecount()
 
         embed = discord.Embed(colour=discord.Color.gold())
-        embed.add_field(name='__**Bot info:**__',
-                        value=f'**Uptime:** {self.bot.utils.format_time(seconds=uptime, friendly=True)}\n'
-                              f'**Users:** {len(self.bot.users)}\n **Guilds:** {len(self.bot.guilds)}\n'
-                              f'**Shards:** {len(self.bot.shards)}')
+        embed.add_field(name='Bot info:',
+                        value=f'`Uptime:` {uptime}\n`Guilds:`{len(self.bot.guilds)}\n'
+                              f'`Shards:` {len(self.bot.shards)}\n`Users:` {len(self.bot.users)}\n')
         embed.add_field(name='\u200B', value='\u200B')
-        embed.add_field(name='__**Bot stats:**__',
-                        value=f'**Discord.py Version:** {discord.__version__}\n**Commands:** {len(self.bot.commands)}\n'
-                              f'**Extensions:** {len(self.bot.extensions)}\n**Cogs:** {len(self.bot.cogs)}')
+        embed.add_field(name='Bot stats:',
+                        value=f'`Discord.py:` {discord.__version__}\n`Extensions:` {len(self.bot.extensions)}\n'
+                              f'`Commands:` {len(self.bot.commands)}\n`Cogs:` {len(self.bot.cogs)}')
 
-        embed.add_field(name='__**Code:**__',
-                        value=f'**Functions:** {functions}\n**Classes:** {classes}\n'
-                              f'**Lines:** {lines}\n**Files:** {files}\n')
+        embed.add_field(name='Code:',
+                        value=f'`Functions:` {functions}\n`Classes:` {classes}\n'
+                              f'`Lines:` {lines}\n`Files:` {files}\n')
         embed.add_field(name='\u200B', value='\u200B')
-        embed.add_field(name='__**Ping:**__',
-                        value=f'**Average:** {average_latency_ms}\n**Latency:** {latency_ms}\n'
-                              f'**Typing:** {typing_ms}\n**Discord:** {discord_ms}')
+        embed.add_field(name='Ping:',
+                        value=f'`Latency:` {latency_ms}\n`Discord:` {discord_ms}\n`Typing:` {typing_ms}')
 
-        embed.add_field(name='__**Links:**__',
-                        value=f'**[Invite me]({self.bot.invite_url}) | [Support server]({self.bot.support_url}) | '
-                              f'[Source code]({self.bot.source_url}) | [Upvote me]({self.bot.upvote_url})**')
+        embed.add_field(name='Links:',
+                        value=f'`[Invite me]({self.bot.invite_url}) | [Support server]({self.bot.support_url}) | '
+                              f'[Source code]({self.bot.source_url}) | [Upvote me]({self.bot.upvote_url})`')
 
         embed.set_footer(text=f'Created on {datetime.strftime(self.bot.user.created_at, "%A %d %B %Y at %H:%M")}')
         return await ctx.send(embed=embed)
@@ -85,25 +84,25 @@ class Information(commands.Cog):
         """
 
         embed = discord.Embed(colour=discord.Color.gold())
-        embed.add_field(name='__**System CPU:**__',
-                        value=f'**Cores:** {psutil.cpu_count()}\n'
-                              f'**Usage:** {psutil.cpu_percent(interval=0.1)}%\n'
-                              f'**Frequency:** {round(psutil.cpu_freq().current, 2)} Mhz')
+        embed.add_field(name='System CPU:',
+                        value=f'`Frequency:` {round(psutil.cpu_freq().current, 2)} Mhz\n'
+                              f'`Cores:` {psutil.cpu_count()}\n'
+                              f'`Usage:` {psutil.cpu_percent(interval=0.1)}%')
         embed.add_field(name='\u200B', value='\u200B')
-        embed.add_field(name='__**System Memory:**__',
-                        value=f'**Total:** {round(psutil.virtual_memory().total / 1048576)} MB\n'
-                              f'**Used:** {round(psutil.virtual_memory().used / 1048576)} MB\n'
-                              f'**Available:** {round(psutil.virtual_memory().available / 1048576)} Mb')
+        embed.add_field(name='System Memory:',
+                        value=f'`Available:` {round(psutil.virtual_memory().available / 1048576)} MB\n'
+                              f'`Total:` {round(psutil.virtual_memory().total / 1048576)} MB\n'
+                              f'`Used:` {round(psutil.virtual_memory().used / 1048576)} MB')
 
-        embed.add_field(name='__**System Disk:**__',
-                        value=f'**Total:** {round(psutil.disk_usage("/").total / 1073741824, 2)} GB\n'
-                              f'**Used:** {round(psutil.disk_usage("/").used / 1073741824, 2)} GB\n'
-                              f'**Free:** {round(psutil.disk_usage("/").free / 1073741824, 2)} GB')
+        embed.add_field(name='System Disk:',
+                        value=f'`Total:` {round(psutil.disk_usage("/").total / 1073741824, 2)} GB\n'
+                              f'`Used:` {round(psutil.disk_usage("/").used / 1073741824, 2)} GB\n'
+                              f'`Free:` {round(psutil.disk_usage("/").free / 1073741824, 2)} GB')
         embed.add_field(name='\u200B', value='\u200B')
-        embed.add_field(name='__**Process information:**__',
-                        value=f'**Memory usage:** {round(self.bot.process.memory_full_info().rss / 1048576, 2)} MB\n'
-                              f'**CPU usage:** {self.bot.process.cpu_percent(interval=None)}%\n'
-                              f'**Threads:** {self.bot.process.num_threads()}')
+        embed.add_field(name='Process information:',
+                        value=f'`Memory usage:` {round(self.bot.process.memory_full_info().rss / 1048576, 2)} MB\n'
+                              f'`CPU usage:` {self.bot.process.cpu_percent(interval=None)}%\n'
+                              f'`Threads:` {self.bot.process.num_threads()}')
 
         return await ctx.send(embed=embed)
 
@@ -113,10 +112,9 @@ class Information(commands.Cog):
         Display the bots latency.
         """
 
-        latency_ms, average_latency_ms, typing_ms, discord_ms = await self.bot.utils.ping(ctx)
-        table = tabulate(tabular_data=[['Type', 'Ping'],
-                                       ['Average', average_latency_ms], ['Latency', latency_ms],
-                                       ['Typing', typing_ms], ['Discord', discord_ms]],
+        latency_ms, typing_ms, discord_ms = await self.bot.utils.ping(ctx)
+        table = tabulate(tabular_data=[['Type', 'Ping'], ['Latency', latency_ms],
+                                       ['Discord', discord_ms], ['Typing', typing_ms]],
                          headers='firstrow', tablefmt='psql')
         return await ctx.send(f'```py\n{table}\n```')
 
@@ -165,32 +163,31 @@ class Information(commands.Cog):
 
         statuses = self.bot.utils.guild_member_status(ctx.guild)
 
-        embed = discord.Embed(colour=discord.Color.gold())
-        embed.title = f"{ctx.guild.name}'s stats and information."
+        embed = discord.Embed(colour=discord.Color.gold(), title=f"{ctx.guild.name}'s stats and information.")
         embed.description = ctx.guild.description if ctx.guild.description else None
-        embed.add_field(name='__**General information:**__',
-                        value=f'**Owner:** {ctx.guild.owner}\n'
-                              f'**Created at:** {datetime.strftime(ctx.guild.created_at, "%A %d %B %Y at %H:%M")}\n'
-                              f'**Members:** {ctx.guild.member_count} |'
+        embed.add_field(name='General information:',
+                        value=f'`Owner:` {ctx.guild.owner}\n'
+                              f'`Created at:` {datetime.strftime(ctx.guild.created_at, "%A %d %B %Y at %H:%M")}\n'
+                              f'`Members:` {ctx.guild.member_count} |'
                               f'<:online:627627415224713226>{statuses["online"]} |'
                               f'<:away:627627415119724554>{statuses["idle"]} |'
                               f'<:dnd:627627404784828416>{statuses["dnd"]} |'
                               f'<:offline:627627415144890389>{statuses["offline"]}\n'
-                              f'**Nitro Tier:** {ctx.guild.premium_tier} | '
-                              f'**Boosters:** {ctx.guild.premium_subscription_count}\n'
-                              f'**File Size:** {round(ctx.guild.filesize_limit / 1048576)} MB | '
-                              f'**Bitrate:** {round(ctx.guild.bitrate_limit / 1000)} kbps | '
-                              f'**Emoji:** {ctx.guild.emoji_limit}\n'
-                              f'**Content filter level:** {self.bot.utils.content_filter_level(ctx.guild)} | '
-                              f'**2FA:** {self.bot.utils.mfa_level(ctx.guild)}\n'
-                              f'**Verification level:** {self.bot.utils.verification_level(ctx.guild)}\n', inline=False)
+                              f'`Nitro Tier:` {ctx.guild.premium_tier} | '
+                              f'`Boosters:` {ctx.guild.premium_subscription_count}\n'
+                              f'`File Size:` {round(ctx.guild.filesize_limit / 1048576)} MB | '
+                              f'`Bitrate:` {round(ctx.guild.bitrate_limit / 1000)} kbps | '
+                              f'`Emoji:` {ctx.guild.emoji_limit}\n'
+                              f'`Content filter level:` {self.bot.utils.content_filter_level(ctx.guild)} | '
+                              f'`2FA:` {self.bot.utils.mfa_level(ctx.guild)}\n'
+                              f'`Verification level:` {self.bot.utils.verification_level(ctx.guild)}\n', inline=False)
 
-        embed.add_field(name='__**Channels:**__',
-                        value=f'**AFK timeout:** {int(ctx.guild.afk_timeout / 60)}m | '
-                              f'**AFK channel:** {ctx.guild.afk_channel}\n'
-                              f'**Text channels:** {len(ctx.guild.text_channels)} | '
-                              f'**Voice channels:** {len(ctx.guild.voice_channels)}\n'
-                              f'**Voice region:** {self.bot.utils.guild_region(ctx.guild)}\n', inline=False)
+        embed.add_field(name='`Channels:`',
+                        value=f'`AFK timeout:` {int(ctx.guild.afk_timeout / 60)}m | '
+                              f'`AFK channel:` {ctx.guild.afk_channel}\n'
+                              f'`Text channels:` {len(ctx.guild.text_channels)} | '
+                              f'`Voice channels:` {len(ctx.guild.voice_channels)}\n'
+                              f'`Voice region:` {self.bot.utils.guild_region(ctx.guild)}\n', inline=False)
 
         embed.set_thumbnail(url=self.bot.utils.guild_icon(ctx.guild))
         embed.set_image(url=self.bot.utils.guild_banner(ctx.guild))
@@ -233,16 +230,16 @@ class Information(commands.Cog):
 
         embed = discord.Embed(colour=self.bot.utils.member_colour(ctx.author))
         embed.title = f"{member}'s stats and information."
-        embed.add_field(name='__**General information:**__',
-                        value=f'**Discord Name:** {member}\n'
-                              f'**Created at:** {datetime.strftime(member.created_at, "%A %d %B %Y at %H:%M")}\n'
-                              f'**Status:** {self.bot.utils.member_status(member)}\n'
-                              f'**Activity:** {self.bot.utils.member_activity(member)}', inline=False)
+        embed.add_field(name='General information:',
+                        value=f'`Discord Name:` {member}\n'
+                              f'`Created at:` {datetime.strftime(member.created_at, "%A %d %B %Y at %H:%M")}\n'
+                              f'`Status:` {self.bot.utils.member_status(member)}\n'
+                              f'`Activity:` {self.bot.utils.member_activity(member)}', inline=False)
 
-        embed.add_field(name='__**Server-related information:**__',
-                        value=f'**Nickname:** {member.nick}\n'
-                              f'**Joined server:** {datetime.strftime(member.joined_at, "%A %d %B %Y at %H:%M")}\n'
-                              f'**Top role:** {member.top_role.mention}', inline=False)
+        embed.add_field(name='Server-related information:',
+                        value=f'`Joined server:` {datetime.strftime(member.joined_at, "%A %d %B %Y at %H:%M")}\n'
+                              f'`Nickname:` {member.nick}\n'
+                              f'`Top role:` {member.top_role.mention}', inline=False)
 
         embed.set_thumbnail(url=member.avatar_url_as(format='png'))
         embed.set_footer(text=f'ID: {member.id}')
