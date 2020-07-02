8dff87ee4287e8d465cedfa8d6a06b21b9e758a0
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-07-03 00:11:05 +0100

    reset cooldown on command error

--- Life/cogs/events.py
+++ Life/cogs/events.py
@@ -125,17 +125,47 @@ class Events(commands.Cog):
         prefix = self.bot.config.prefix
         command = ctx.command
 
-        if ctx.author.id in self.bot.owner_ids:
-            ctx.command.reset_cooldown(ctx) if ctx.command else None
-
         if isinstance(error, commands.CommandNotFound):
             return  # Ignore this exception
 
-        elif isinstance(error, commands.BotMissingPermissions):
+        elif isinstance(error, commands.CommandOnCooldown):
+
+            if ctx.author.id in self.bot.owner_ids:
+                command.reset_cooldown(ctx)
+                return await command.reinvoke(ctx)
+
+            cooldowns = {
+                commands.BucketType.default: f'for the whole bot.',
+                commands.BucketType.user: f'for you.',
+                commands.BucketType.guild: f'for this server.',
+                commands.BucketType.channel: f'for this channel.',
+                commands.BucketType.member: f'cooldown for you.',
+                commands.BucketType.category: f'for this channel category.',
+                commands.BucketType.role: f'for your role.'
+            }
+            return await ctx.send(f'The command `{command}` is on cooldown {cooldowns[error.cooldown.type]} '
+                                  f'You can retry in `{self.bot.utils.format_time(error.retry_after, friendly=True)}`')
+
+        elif isinstance(error, commands.MaxConcurrencyReached):
+            cooldowns = {
+                commands.BucketType.default: f'.',
+                commands.BucketType.user: f' per user.',
+                commands.BucketType.guild: f' per server.',
+                commands.BucketType.channel: f' per channel.',
+                commands.BucketType.member: f' per member.',
+                commands.BucketType.category: f' per channel category.',
+                commands.BucketType.role: f' per role.'
+            }
+            return await ctx.send(f'The command `{command}` is already being ran at its maximum of '
+                                  f'{error.number} time(s){cooldowns[error.per]} Retry a bit later.')
+
+        command.reset_cooldown(ctx)
+
+        if isinstance(error, commands.BotMissingPermissions):
             permissions = '\n'.join([f'> {permission}' for permission in error.missing_perms])
             message = f'I am missing the following permissions required to run the command `{command}`.' \
                       f'\n{permissions}'
-            try:   # Special case for when the bot doesnt have permissions to send messages.
+            try:
                 return await ctx.send(message)
             except discord.Forbidden:
                 try:
@@ -152,32 +182,6 @@ class Events(commands.Cog):
             return await ctx.send(f'You missed the `{error.param.name}` parameter for the command `{command}`. '
                                   f'Use `{self.bot.config.prefix}help {command}` for more information on what '
                                   f'parameters to use.')
-
-        elif isinstance(error, commands.CommandOnCooldown):
-            cooldowns = {
-                commands.BucketType.default: f'for the whole bot.',
-                commands.BucketType.user: f'for you.',
-                commands.BucketType.guild: f'for this server.',
-                commands.BucketType.channel: f'for this channel.',
-                commands.BucketType.member: f'cooldown for you.',
-                commands.BucketType.category: f'for this channel category.',
-                commands.BucketType.role: f'for your role.'
-            }
-            return await ctx.send(f'The command `{command}` is on cooldown {cooldowns[error.cooldown.type]}'
-                                  f'You can retry in `{self.bot.utils.format_time(error.retry_after, friendly=True)}`')
-
-        elif isinstance(error, commands.MaxConcurrencyReached):
-            cooldowns = {
-                commands.BucketType.default: f'.',
-                commands.BucketType.user: f' per user.',
-                commands.BucketType.guild: f' per server.',
-                commands.BucketType.channel: f' per channel.',
-                commands.BucketType.member: f' per member.',
-                commands.BucketType.category: f' per channel category.',
-                commands.BucketType.role: f' per role.'
-            }
-            return await ctx.send(f'The command `{command}` is already being ran at its maximum of '
-                                  f'{error.number} time(s){cooldowns[error.per]} Retry a bit later.')
 
         error_messages = {
             exceptions.ArgumentError: f'{error}',
@@ -206,14 +210,12 @@ class Events(commands.Cog):
         await ctx.send(f'Something went wrong while executing that command. Please use `{prefix}support` for more '
                        f'help/information.')
 
-        ctx.command.reset_cooldown(ctx) if ctx.command else None
-
         guild = f'`Guild:` {ctx.guild}\n' if ctx.guild else ''
         guild_id = f'`Guild ID:` {ctx.guild.id}\n' if ctx.guild else ''
         time = datetime.strftime(ctx.message.created_at, "%A %d %B %Y at %H:%M:%S")
         info = f'`Message content:` {ctx.message.content}\n{guild}{guild_id}`Channel:` {ctx.channel}\n' \
                f'`Channel ID:` {ctx.channel.id}\n `Time`: {time}'
-        embed = discord.Embed(colour=discord.Colour.gold(), description=f'Error in command `{ctx.command}`')
+        embed = discord.Embed(colour=discord.Colour.gold(), description=f'Error in command `{command}`')
         embed.set_author(name=ctx.author, icon_url=self.bot.utils.member_avatar(ctx.author))
         embed.add_field(name='Info:', value=info)
         await self.bot.error_channel.send(embed=embed)
@@ -224,7 +226,7 @@ class Events(commands.Cog):
         traceback.print_exception(type(error), error, error.__traceback__)
 
     @commands.Cog.listener()
-    async def on_socket_response(self, message):
+    async def on_socket_response(self, message: dict):
 
         event = message.get('t', 'None')
         if event is not None:
