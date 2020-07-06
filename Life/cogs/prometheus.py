2b7daa625749bacb27a7496dc685e6a422102405
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-07-06 22:33:18 +0100

    added member logging

--- Life/cogs/prometheus.py
+++ Life/cogs/prometheus.py
@@ -15,22 +15,40 @@ You should have received a copy of the GNU Affero General Public License along w
 
 from discord.ext import commands
 import prometheus_client
+import discord
 
 
 class Prometheus(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.bot.dispatch_events = prometheus_client.Counter("dispatch_events", documentation="Dispatch Events",
-                                                         namespace="life", labelnames=['event'])
+        self.bot.socket_events = prometheus_client.Counter('socket_events', documentation='Socket events',
+                                                           namespace='life', labelnames=['event'])
+        self.bot.counts = prometheus_client.Gauge('counts', documentation='Life counts',
+                                                  namespace='life', labelnames=['count'])
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(sum([len(guild.members) for guild in self.bot.guilds]))
+        self.bot.counts.labels(count='members').set(sum([len(guild.members) for guild in self.bot.guilds]))
+        self.bot.counts.labels(count='users').set(len(self.bot.users))
 
     @commands.Cog.listener()
     async def on_socket_response(self, message: dict):
 
         event = message.get('t', 'None')
         if event is not None:
-            self.bot.dispatch_events.labels(event=event).inc()
+            self.bot.socket_events.labels(event=event).inc()
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        self.bot.counts.labels(count='members').inc()
+        self.bot.counts.labels(count='users').inc()
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member: discord.Member):
+        self.bot.counts.labels(count='members').dec()
+        self.bot.counts.labels(count='users').dec()
 
 def setup(bot):
     bot.add_cog(Prometheus(bot))
