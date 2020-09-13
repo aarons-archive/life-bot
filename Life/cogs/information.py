3c1e072b56a50d3c1e23f192d8037a9fe089da70
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-09-13 22:51:20 +0100

    fix issue with user command on people with animated avatars

--- Life/cogs/information.py
+++ Life/cogs/information.py
@@ -26,95 +26,14 @@ import psutil
 from discord.ext import commands
 from discord.ext.alternatives import guild_converter
 
+from bot import Life
 from utilities import context, converters, exceptions
 
 
 class Information(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: Life):
         self.bot = bot
-
-        self.bot.process = psutil.Process()
-
-        self.bot.support_url = 'https://discord.gg/xP8xsHr'
-        self.bot.source_url = 'https://github.com/MyNameBeMrRandom/Life'
-        self.bot.upvote_url = 'https://top.gg/bot/628284183579721747'
-        self.bot.invite_url = f'https://discord.com/oauth2/authorize?client_id=628284183579721747&scope=bot&permissions=103926848'
-
-        self.bot.colours = {
-            discord.Status.online: 0x008000,
-            discord.Status.idle: 0xFF8000,
-            discord.Status.dnd: 0xFF0000,
-            discord.Status.offline: 0x808080,
-            discord.Status.invisible: 0x808080,
-        }
-
-        self.bot.mfa_levels = {
-            0: 'Not required',
-            1: 'Required'
-        }
-
-        self.bot.verification_levels = {
-            discord.VerificationLevel.none: 'None - No criteria set.',
-            discord.VerificationLevel.low: 'Low - Must have a verified email.',
-            discord.VerificationLevel.medium: 'Medium - Must have a verified email and be registered on discord for '
-                                              'more than 5 minutes.',
-            discord.VerificationLevel.high: 'High - Must have a verified email, be registered on discord for more '
-                                            'than 5 minutes and be a member of the guild for more then 10 minutes.',
-            discord.VerificationLevel.extreme: 'Extreme - Must have a verified email, be registered on discord for '
-                                               'more than 5 minutes, be a member of the guild for more then 10 minutes '
-                                               'and a have a verified phone number.'
-        }
-
-        self.bot.content_filter_levels = {
-            discord.ContentFilter.disabled: 'None',
-            discord.ContentFilter.no_role: 'No roles',
-            discord.ContentFilter.all_members: 'All members',
-        }
-
-        self.bot.badges = {
-            'staff': '<:staff:738961032109752441>',
-            'partner': '<:partner:738961058613559398>',
-            'hypesquad': '<:hypesquad:738960840375664691>',
-            'bug_hunter': '<:bug_hunter:738961014275571723>',
-            'bug_hunter_level_2': '<:bug_hunter_level_2:739390267949580290>',
-            'hypesquad_bravery': '<:hypesquad_bravery:738960831596855448>',
-            'hypesquad_brilliance': '<:hypesquad_brilliance:738960824327995483>',
-            'hypesquad_balance': '<:hypesquad_balance:738960813460684871>',
-            'early_supporter': '<:early_supporter:738961113219203102>',
-            'system': '<:system_1:738960703284576378><:system_2:738960703288770650>',
-            'verified_bot': '<:verified_bot_1:738960728022581258><:verified_bot_2:738960728102273084>',
-            'verified_bot_developer': '<:verified_bot_developer:738961212250914897>',
-        }
-
-        self.bot.features = {
-            'VIP_REGIONS': 'Has VIP voice regions',
-            'VANITY_URL': 'Can have vanity invite',
-            'INVITE_SPLASH': 'Can have invite splash',
-            'VERIFIED': 'Is verified server',
-            'PARTNERED': 'Is partnered server',
-            'MORE_EMOJI': 'Can have 50+ emoji',
-            'DISCOVERABLE': 'Is discoverable',
-            'FEATURABLE': 'Is featurable',
-            'COMMERCE': 'Can have store channels',
-            'PUBLIC': 'Is public',
-            'NEWS': 'Can have news channels',
-            'BANNER': 'Can have banner',
-            'ANIMATED_ICON': 'Can have animated icon',
-            'PUBLIC_DISABLED': 'Can not be public',
-            'WELCOME_SCREEN_ENABLED': 'Can have welcome screen'
-        }
-
-        self.bot.channel_emojis = {
-            'text': '<:text:739399497200697465>',
-            'text_locked': '<:text_locked:739399496953364511>',
-            'text_nsfw': '<:text_nsfw:739399497251160115>',
-            'news': '<:news:739399496936718337>',
-            'news_locked': '<:news_locked:739399497062416435>',
-            'voice': '<:voice:739399497221931058>',
-            'voice_locked': '<:voice_locked:739399496924135476>',
-            'category': '<:category:738960756233601097>'
-        }
 
     @commands.command(name='info', aliases=['about'])
     async def info(self, ctx: context.Context):
@@ -130,7 +49,7 @@ class Information(commands.Cog):
         Display the bots stats.
         """
 
-        uptime = self.bot.utils.format_time(seconds=round(time.time() - self.bot.start_time), friendly=True)
+        uptime = self.bot.utils.format_seconds(seconds=round(time.time() - self.bot.start_time), friendly=True)
 
         files, functions, lines, classes = 0, 0, 0, 0
         docstring = False
@@ -181,8 +100,8 @@ class Information(commands.Cog):
                         value=f'`Latency:` {round(self.bot.latency * 1000)}ms')
 
         embed.add_field(name='Links:',
-                        value=f'[Invite me]({self.bot.invite_url}) | [Support server]({self.bot.support_url}) | '
-                              f'[Source code]({self.bot.source_url}) | [Upvote me]({self.bot.upvote_url})')
+                        value=f'[Invite me]({self.bot.invite}) | [Support server]({self.bot.support}) | '
+                              f'[Source code]({self.bot.github})')
 
         embed.set_footer(text=f'Created on {datetime.strftime(self.bot.user.created_at, "%A %d %B %Y at %H:%M")}')
         return await ctx.send(embed=embed)
@@ -231,7 +150,7 @@ class Information(commands.Cog):
         """
 
         return await ctx.send(f'If you have any problems with the bot or if you have any suggestions/feedback be sure to join the support server using this '
-                              f'link {self.bot.support_url}')
+                              f'link {self.support}')
 
     @commands.command(name='invite')
     async def support(self, ctx: context.Context):
@@ -239,7 +158,7 @@ class Information(commands.Cog):
         Get a link the invite the bot.
         """
 
-        return await ctx.send(f'You can invite the bot here <{self.bot.invite_url}>')
+        return await ctx.send(f'You can invite the bot here <{self.bot.invite}>')
 
     @commands.command(name='source')
     async def source(self, ctx: context.Context, *, command: str = None):
@@ -250,7 +169,7 @@ class Information(commands.Cog):
         """
 
         if command is None:
-            return await ctx.send(f'<{self.bot.source_url}>')
+            return await ctx.send(f'<{self.bot.github}>')
 
         command = self.bot.get_command(command)
         if command is None:
@@ -267,7 +186,7 @@ class Information(commands.Cog):
         lines, first_line = inspect.getsourcelines(command_obj)
         last_line = first_line + (len(lines) - 1)
 
-        link = f'<{self.bot.source_url}/blob/master/Life/{file_path}#L{first_line}-L{last_line}>'
+        link = f'<{self.bot.github}/blob/master/Life/{file_path}#L{first_line}-L{last_line}>'
         return await ctx.send(link)
 
     @commands.command(name='icon')
@@ -354,7 +273,7 @@ class Information(commands.Cog):
         statuses = collections.Counter([member.status for member in guild.members])
 
         features = []
-        for feature, description in self.bot.features.items():
+        for feature, description in self.bot.utils.features.items():
             if feature in guild.features:
                 features.append(f'<:tick:739315349715026001> {description}')
             else:
@@ -362,14 +281,14 @@ class Information(commands.Cog):
 
         embed = discord.Embed(colour=ctx.colour, title=f"{guild.name}'s information.")
         embed.description = f'`Owner:` {guild.owner}\n' \
-                            f'`Created on:` {datetime.strftime(guild.created_at, self.bot.time_format)}\n' \
+                            f'`Created on:` {self.bot.utils.format_datetime(time=guild.created_at)}\n' \
                             f'`Created:` {humanize.precisedelta(datetime.utcnow() - guild.created_at, suppress=["seconds", "minutes"], format="%0.0f")} ago\n' \
                             f'`Members:` {guild.member_count} | ' \
                             f'<:online:737824551471284356>{statuses[discord.Status.online]} | <:away:627627415119724554>{statuses[discord.Status.idle]} | ' \
                             f'<:dnd:627627404784828416>{statuses[discord.Status.dnd]} | <:offline:627627415144890389>{statuses[discord.Status.offline]}\n' \
-                            f'`Content filter level:` {self.bot.content_filter_levels[ctx.guild.explicit_content_filter]} | ' \
-                            f'`2FA:` {self.bot.mfa_levels[ctx.guild.mfa_level]}\n' \
-                            f'`Verification level:` {self.bot.verification_levels[ctx.guild.verification_level]}\n'
+                            f'`Content filter level:` {self.bot.utils.content_filter_levels[ctx.guild.explicit_content_filter]} | ' \
+                            f'`2FA:` {self.bot.utils.mfa_levels[ctx.guild.mfa_level]}\n' \
+                            f'`Verification level:` {self.bot.utils.verification_levels[ctx.guild.verification_level]}\n'
 
         embed.add_field(name='Boost information:',
                         value=f'`Nitro Tier:` {ctx.guild.premium_tier} | `Boosters:` {ctx.guild.premium_subscription_count} | '
@@ -403,13 +322,13 @@ class Information(commands.Cog):
         categories = [category for category in guild.channels if isinstance(category, discord.CategoryChannel)]
 
         for channel in sorted(channels, key=lambda channel: channel.position):
-            entries.append(f'{self.bot.utils.channel_emoji(channel)}{channel}')
+            entries.append(f'{self.bot.utils.channel_emoji(channel=channel)}{channel}')
 
         for category in sorted(categories, key=lambda category: category.position):
             entries.append(f'<:category:738960756233601097> **{category}**')
             for channel in category.channels:
                 space = '\u200b ' * 4
-                entries.append(f'{space}{self.bot.utils.channel_emoji(channel)}{channel}')
+                entries.append(f'{space}{self.bot.utils.channel_emoji(channel=channel)}{channel}')
 
         return await ctx.paginate_embed(entries=entries, per_page=30, title='List of channels, categories and voice channels.')
 
@@ -430,7 +349,7 @@ class Information(commands.Cog):
 
         if user.is_avatar_animated():
             embed.description += f' | [GIF]({user.avatar_url_as(format="gif")})'
-            embed.set_image(url=str(user.icon_url_as(format='gif')))
+            embed.set_image(url=str(user.avatar_url_as(format='gif')))
 
         return await ctx.send(embed=embed)
 
@@ -443,11 +362,11 @@ class Information(commands.Cog):
         """
 
         if not user:
-            user = self.bot.get_user(ctx.author.id)
+            user = await self.bot.fetch_user(ctx.author.id)
 
         embed = discord.Embed(colour=ctx.colour, title=f'{user}\'s information:')
         embed.description = f'`Discord name:` {user}\n' \
-                            f'`Created on:` {datetime.strftime(user.created_at, self.bot.time_format)}\n' \
+                            f'`Created on:` {self.bot.utils.format_datetime(time=user.created_at)}\n' \
                             f'`Created:` {humanize.precisedelta(datetime.utcnow() - user.created_at, suppress=["seconds", "minutes"], format="%0.0f")} ago\n' \
                             f'`Badges:` {self.bot.utils.badges(person=user)}\n' \
                             f'`Bot:` {str(user.bot).replace("True", "Yes").replace("False", "No")}'
@@ -466,9 +385,9 @@ class Information(commands.Cog):
         if member is None:
             member = ctx.author
 
-        embed = discord.Embed(colour=self.bot.colours[member.status], title=f'{member}\'s information.')
+        embed = discord.Embed(colour=self.bot.utils.colours[member.status], title=f'{member}\'s information.')
         embed.description = f'`Discord Name:` {member} {"<:owner:738961071729278987>" if member.id == member.guild.owner.id else ""}\n' \
-                            f'`Created on:` {datetime.strftime(member.created_at, self.bot.time_format)}\n' \
+                            f'`Created on:` {self.bot.utils.format_datetime(time=member.created_at)}\n' \
                             f'`Created:` {humanize.precisedelta(datetime.utcnow() - member.created_at, suppress=["seconds", "minutes"], format="%0.0f")} ago\n' \
                             f'`Badges:` {self.bot.utils.badges(person=member)}\n' \
                             f'`Status:` {member.status.name.replace("dnd", "Do Not Disturb").title()}' \
@@ -478,7 +397,7 @@ class Information(commands.Cog):
 
         embed.add_field(name='Server related information:',
                         value=f'`Server nickname:` {member.nick}\n'
-                              f'`Joined on:` {datetime.strftime(member.joined_at, self.bot.time_format)}\n'
+                              f'`Joined on:` {self.bot.utils.format_datetime(time=member.joined_at)}\n'
                               f'`Joined:` {humanize.precisedelta(datetime.utcnow() - member.joined_at, suppress=["seconds", "minutes"], format="%0.0f")} ago\n'
                               f'`Top role:` {member.top_role.mention}\n'
                               f'`Role count:` {len(member.roles) - 1}', inline=False)
