40127f9ffbceed19d89c7303db33f62f77ba1136
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-09-21 22:37:53 +0100

    added time xog

---
+++ Life/cogs/time.py
@@ -0,0 +1,76 @@
+import datetime as dt
+
+import discord
+import pytz
+from discord.ext import commands
+
+from bot import Life
+from utilities import context, converters, exceptions
+
+
+class Time(commands.Cog):
+
+    def __init__(self, bot: Life) -> None:
+        self.bot = bot
+
+    @commands.command(name='timezones', aliases=['tzs'])
+    async def timezones(self, ctx: context.Context) -> None:
+        """
+        Displays a list of timezones that can be used with the bot.
+        """
+
+        await ctx.paginate_embed(entries=pytz.all_timezones, per_page=20, title='Available timezones:', url=f'{self.bot.dashboard}timezones')
+
+    @commands.group(name='time', invoke_without_command=True)
+    async def time(self, ctx: context.Context, *, timezone: str = None) -> None:
+        """
+        Displays the time of the member or timezone provided.
+
+        `timezone`: The timezone or members name, id or mention that you want to get.
+        """
+
+        if not timezone:
+            member = ctx.author
+            timezone = ctx.user_config.pytz
+        else:
+            try:
+                member = ctx.author
+                timezone = await self.bot.timezone_converter.convert(ctx=ctx, argument=timezone)
+            except exceptions.ArgumentError as error:
+                try:
+                    member = await self.bot.member_converter.convert(ctx=ctx, argument=timezone)
+                    timezone = self.bot.get_user_config(user=member).pytz
+                except commands.BadArgument:
+                    raise exceptions.ArgumentError(str(error))
+
+        datetime = self.bot.utils.format_datetime(datetime=dt.datetime.now(tz=timezone))
+
+        embed = discord.Embed(colour=ctx.user_config.colour, title=f'Time in {timezone.zone} {f"({member})" if member else ""}', description=f'```py\n{datetime}\n```')
+        await ctx.send(embed=embed)
+
+    @time.command(name='set')
+    async def time_set(self, ctx: context.Context, *, timezone: converters.TimezoneConverter):
+        """
+        Sets your timezone to the one specified.
+
+        See https://dashboard.mrrandom.xyz/timezones/ for a full list of avaiable timezones.
+
+        `timezone`: The timezone to use.
+        """
+
+        await self.bot.set_user_config(user=ctx.author, attribute='timezone', value=timezone.zone)
+        await ctx.send(f'Set your timezone to `{timezone.zone}`.')
+
+    @time.command(name='clear')
+    async def time_clear(self, ctx: context.Context):
+        """
+        Sets your timezone back to default (UTC).
+        """
+
+        await self.bot.set_user_config(user=ctx.author, attribute='timezone', value='UTC')
+        await ctx.send(f'Reset your timezone.')
+
+
+def setup(bot):
+    bot.add_cog(Time(bot))
+
--- Life/utilities/objects.py
+++ Life/utilities/objects.py
@@ -12,54 +12,74 @@ You should have received a copy of the GNU Affero General Public License along w
 """
 
 import discord
+import pytz
 
 
 class DefaultGuildConfig:
 
-    __slots__ = ('prefixes', 'colour')
+    __slots__ = ('prefixes', 'colour', 'starboard_channel', 'starboard_threshold')
 
     def __init__(self) -> None:
 
-        self.prefixes = []
         self.colour = discord.Colour.gold()
+        self.prefixes = []
+        self.starboard_channel = None
+        self.starboard_threshold = 3
 
     def __repr__(self) -> str:
-        return f'<DefaultGuildConfig prefixes={self.prefixes} colour={self.colour}>'
+        return f'<DefaultGuildConfig colour={self.colour} prefixes={self.prefixes} >'
 
 
 class GuildConfig:
 
-    __slots__ = ('prefixes', 'colour')
+    __slots__ = ('prefixes', 'colour', 'starboard_channel', 'starboard_threshold')
 
     def __init__(self, data: dict) -> None:
 
-        self.prefixes = data.get('prefixes')
         self.colour = discord.Colour(int(data.get('colour'), 16))
+        self.prefixes = data.get('prefixes')
+        self.starboard_channel = data.get('starboard_channel')
+        self.starboard_threshold = data.get('starboard_threshold')
 
     def __repr__(self) -> str:
-        return f'<GuildConfig prefixes={self.prefixes} colour={self.colour}>'
+        return f'<GuildConfig colour={self.colour} prefixes={self.prefixes} >'
 
 
 class DefaultUserConfig:
 
-    __slots__ = 'colour'
+    __slots__ = ('colour', 'money', 'timezone', 'timezone_private')
 
     def __init__(self) -> None:
 
         self.colour = discord.Colour.gold()
+        self.money = 0
+        self.timezone = 'UTC'
+        self.timezone_private = False
 
     def __repr__(self) -> str:
-        return f'<DefaultUserConfig colour={self.colour}>'
+        timezone = f'timezone=\'{self.timezone}\'' if self.timezone_private is False else ''
+        return f'<DefaultUserConfig colour=\'{self.colour}\' money={self.money} timezone_private={self.timezone_private} {timezone}>'
+
+    @property
+    def pytz(self):
+        return pytz.timezone(self.timezone)
 
 
 class UserConfig:
 
-    __slots__ = 'colour'
+    __slots__ = ('colour', 'money', 'timezone', 'timezone_private')
 
     def __init__(self, data: dict) -> None:
 
         self.colour = discord.Colour(int(data.get('colour'), 16))
+        self.money = data.get('money')
+        self.timezone = data.get('timezone')
+        self.timezone_private = data.get('timezone_private')
 
     def __repr__(self) -> str:
-        return f'<UserConfig colour={self.colour}>'
+        timezone = f'timezone=\'{self.timezone}\'' if self.timezone_private is False else ''
+        return f'<UserConfig colour=\'{self.colour}\' money={self.money} timezone_private={self.timezone_private} {timezone}>'
 
+    @property
+    def pytz(self):
+        return pytz.timezone(self.timezone)
