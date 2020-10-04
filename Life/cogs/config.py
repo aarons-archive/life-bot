fbf1ca25e1f7e476dbef12e852c1d3b1f020f1a0
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-10-04 23:07:25 +0100

    refactor config cog
    - Changed settings name to config
    - Rewrite colour command
    - Rest is to be done

--- Life/cogs/config.py
+++ Life/cogs/config.py
@@ -23,64 +23,45 @@ class Config(commands.Cog):
     def __init__(self, bot: Life) -> None:
         self.bot = bot
 
-    @commands.group(name='settings', invoke_without_command=True)
-    async def settings(self, ctx: context.Context) -> None:
+    @commands.group(name='config', invoke_without_command=True)
+    async def config(self, ctx: context.Context) -> None:
         """
-        Display the current server settings.
+        Display the servers config.
         """
 
-        prefixes = await self.bot.get_prefix(ctx.message)
-        prefixes.pop(1)
+        prefixes = ', '.join([f'`{prefix}`' for prefix in ctx.guild_config.prefixes])
 
-        clean_prefixes = [prefixes.pop(0)]
-        clean_prefixes.extend([f'`{prefix}`' for prefix in prefixes])
-
-        embed = discord.Embed(colour=ctx.guild_config.colour, title=f'Current settings:', description=f'`Prefixes:` {", ".join(clean_prefixes)}')
+        embed = discord.Embed(colour=ctx.guild_config.colour, title=f'Guild config:', description=f'`Prefixes:` {prefixes}')
         embed.add_field(name='Embed colour:', value=f'`<---` `{str(ctx.guild_config.colour).upper()}`')
         await ctx.send(embed=embed)
 
-    #
-
-    @settings.group(name='colour', aliases=['color'], invoke_without_command=True)
-    async def settings_colour(self, ctx: context.Context) -> None:
+    @config.group(name='colour', aliases=['color'], invoke_without_command=True)
+    async def config_colour(self, ctx: context.Context, operation: str = None, value: commands.ColourConverter = None) -> None:
         """
         Display the embed colour for this server.
         """
 
-        await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=f'{str(ctx.guild_config.colour).upper()}'))
+        if not operation:
+            await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=str(ctx.guild_config.colour).upper()))
+            return
 
-    @settings_colour.command(name='set')
-    @checks.has_guild_permissions(manage_guild=True)
-    async def config_colour_set(self, ctx: context.Context, *, colour: commands.ColourConverter) -> None:
-        """
-        Set the embed colour for this server.
+        old_colour = ctx.guild_config.colour
 
-        You must have the `manage server` permission to use this command.
+        if operation == 'reset' and value is None:
+            await self.bot.guild_manager.edit_guild_config(guild_id=ctx.guild.id, attribute='colour', operation='reset')
+        elif operation == 'set' and value is not None:
+            await self.bot.guild_manager.edit_guild_config(guild_id=ctx.guild.id, attribute='colour', operation='set', value=f'0x{str(value).strip("#")}')
+        else:
+            raise exceptions.ArgumentError(f'That was not a valid operation. Use `set` or `reset`.')
 
-        `colour`: The colour code to use. Must be in the format `0xFFFFFF`, `0x#FFFFFF`, `FFFFFF` or `#FFFFFF`.
-        """
+        await ctx.send(embed=discord.Embed(colour=old_colour, title=f'Old: {str(old_colour).upper()}'))
+        await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=f'New: {str(ctx.guild_config.colour).upper()}'))
 
-        await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=f'Before: {str(ctx.guild_config.colour).upper()}'))
-        await self.bot.set_guild_config(guild=ctx.guild, attribute='colour', value=f'0x{str(colour).strip("#")}')
-        await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=f'After: {str(ctx.guild_config.colour).upper()}'))
 
-    @settings_colour.command(name='clear', aliases=['default'])
-    @checks.has_guild_permissions(manage_guild=True)
-    async def config_colour_clear(self, ctx: context.Context) -> None:
-        """
-        Clear the embed colour for this server.
 
-        You must have the `manage server` permission to use this command.
-        """
 
-        await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=f'Before: {str(ctx.guild_config.colour).upper()}'))
-        await self.bot.set_guild_config(guild=ctx.guild, attribute='colour', value=f'0x{str(discord.Colour.gold()).strip("#")}')
-        await ctx.send(embed=discord.Embed(colour=ctx.guild_config.colour, title=f'After: {str(ctx.guild_config.colour).upper()}'))
-
-    #
-
-    @settings.group(name='prefix', invoke_without_command=True)
-    async def settings_prefix(self, ctx: context.Context) -> None:
+    @config.group(name='prefix', invoke_without_command=True)
+    async def config_prefix(self, ctx: context.Context) -> None:
         """
         Display a list of available prefixes.
         """
@@ -91,7 +72,7 @@ class Config(commands.Cog):
         entries.extend(f'`{index + 2}.` `{prefix}`' for index, prefix in enumerate(prefixes[2:]))
         await ctx.paginate_embed(entries=entries, per_page=10, title=f'List of usable prefixes in {ctx.guild if ctx.guild else ctx.channel}.')
 
-    @settings_prefix.command(name='add')
+    @config_prefix.command(name='add')
     @checks.has_guild_permissions(manage_guild=True)
     async def config_prefix_add(self, ctx: context.Context, prefix: converters.Prefix) -> None:
         """
@@ -111,7 +92,7 @@ class Config(commands.Cog):
         await self.bot.set_guild_config(guild=ctx.guild, attribute='prefix', value=prefix)
         await ctx.send(f'Added `{prefix}` to this servers prefixes.')
 
-    @settings_prefix.command(name='remove')
+    @config_prefix.command(name='remove')
     @checks.has_guild_permissions(manage_guild=True)
     async def config_prefix_delete(self, ctx: context.Context, prefix: converters.Prefix) -> None:
         """
@@ -130,7 +111,7 @@ class Config(commands.Cog):
         await self.bot.set_guild_config(guild=ctx.guild, attribute='prefix', value=prefix, operation='remove')
         await ctx.send(f'Removed `{prefix}` from this servers prefixes.')
 
-    @settings_prefix.command(name='clear', aliases=['default'])
+    @config_prefix.command(name='clear', aliases=['default'])
     @checks.has_guild_permissions(manage_guild=True)
     async def config_prefix_clear(self, ctx: context.Context) -> None:
         """
