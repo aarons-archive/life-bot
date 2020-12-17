6eded13b27f3c49d85ac67301aa4394a71463718
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-12-17 02:21:29 +0000

    added a roleinfo command i guess

--- Life/cogs/information.py
+++ Life/cogs/information.py
@@ -32,7 +32,7 @@ class Information(commands.Cog):
         self.bot = bot
 
     @commands.command(name='stats')
-    async def stats(self, ctx: context.Context):
+    async def stats(self, ctx: context.Context) -> None:
         """
         Display the bots stats.
         """
@@ -89,10 +89,10 @@ class Information(commands.Cog):
                               f'[Source code]({self.bot.github})')
 
         embed.set_footer(text=f'Created on {self.bot.utils.format_datetime(datetime=self.bot.user.created_at)}')
-        return await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @commands.command(name='system', aliases=['sys'])
-    async def system(self, ctx: context.Context):
+    async def system(self, ctx: context.Context) -> None:
         """
         Display the bots system stats.
         """
@@ -118,35 +118,34 @@ class Information(commands.Cog):
                               f'`CPU usage:` {self.bot.process.cpu_percent(interval=None)}%\n'
                               f'`Threads:` {self.bot.process.num_threads()}')
 
-        return await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @commands.command(name='ping')
-    async def ping(self, ctx: context.Context):
+    async def ping(self, ctx: context.Context) -> None:
         """
         Display the bots latency.
         """
 
-        return await ctx.send(f'{round(self.bot.latency * 1000)}ms')
+        await ctx.send(f'{round(self.bot.latency * 1000)}ms')
 
     @commands.command(name='support')
-    async def support(self, ctx: context.Context):
+    async def support(self, ctx: context.Context) -> None:
         """
         Get an invite link to the bots support server.
         """
 
-        return await ctx.send(f'If you have any problems with the bot or if you have any suggestions/feedback be sure to join the support server using this '
-                              f'link {self.support}')
+        await ctx.send(f'If you have any problems with the bot or if you have any suggestions/feedback be sure to join the support server using this link {self.support}')
 
     @commands.command(name='invite')
-    async def support(self, ctx: context.Context):
+    async def support(self, ctx: context.Context) -> None:
         """
         Get a link the invite the bot.
         """
 
-        return await ctx.send(f'You can invite the bot here <{self.bot.invite}>')
+        await ctx.send(f'You can invite the bot here <{self.bot.invite}>')
 
     @commands.command(name='source')
-    async def source(self, ctx: context.Context, *, command: str = None):
+    async def source(self, ctx: context.Context, *, command: str = None) -> None:
         """
         Get a github link to the source of a command.
 
@@ -154,11 +153,12 @@ class Information(commands.Cog):
         """
 
         if command is None:
-            return await ctx.send(f'<{self.bot.github}>')
+            await ctx.send(f'<{self.bot.github}>')
+            return
 
         command = self.bot.get_command(command)
         if command is None:
-            return await ctx.send('I could not find that command.')
+            raise exceptions.ArgumentError('I could not find that command.')
 
         # noinspection PyProtectedMember
         if isinstance(command, self.bot.help_command._command_impl.__class__):
@@ -172,74 +172,10 @@ class Information(commands.Cog):
         last_line = first_line + (len(lines) - 1)
 
         link = f'<{self.bot.github}/blob/master/Life/{file_path}#L{first_line}-L{last_line}>'
-        return await ctx.send(link)
-
-    @commands.command(name='icon')
-    async def icon(self, ctx: context.Context, *, guild: guild_converter.Guild = None):
-        """
-        Displays a servers icon.
-
-        `guild`: The server of which to get the icon for. Can be it's ID or Name. Defaults to the current server.
-        """
-
-        if not guild:
-            guild = ctx.guild
-
-        if not guild.icon:
-            raise exceptions.ArgumentError(f'The server `{guild}` does not have an icon.')
-
-        embed = discord.Embed(colour=ctx.colour, title=f"{guild.name}'s icon")
-        embed.description = f'[PNG]({guild.icon_url_as(format="png")}) | [JPEG]({guild.icon_url_as(format="jpeg")}) | [WEBP]({guild.icon_url_as(format="webp")})'
-        embed.set_image(url=str(guild.icon_url_as(format='png')))
-
-        if guild.is_icon_animated():
-            embed.description += f' | [GIF]({guild.icon_url_as(format="gif")})'
-            embed.set_image(url=str(guild.icon_url_as(size=1024, format='gif')))
-
-        return await ctx.send(embed=embed)
-
-    @commands.command(name='banner')
-    async def banner(self, ctx: context.Context, *, guild: guild_converter.Guild = None):
-        """
-        Displays a servers banner.
-
-        `guild`: The server of which to get the banner for. Can be it's ID or Name. Defaults to the current server.
-        """
-
-        if not guild:
-            guild = ctx.guild
-
-        if not guild.banner:
-            raise exceptions.ArgumentError(f'The server `{guild}` does not have a banner.')
-
-        embed = discord.Embed(colour=ctx.colour, title=f"{guild.name}'s banner")
-        embed.description = f'[PNG]({guild.banner_url_as(format="png")}) | [JPEG]({guild.banner_url_as(format="jpeg")}) | [WEBP]({guild.banner_url_as(format="webp")})'
-        embed.set_image(url=str(guild.banner_url_as(format='png')))
-
-        return await ctx.send(embed=embed)
-
-    @commands.command(name='splash')
-    async def splash(self, ctx: context.Context, *, guild: guild_converter.Guild = None):
-        """
-        Displays a servers splash.
-
-        `guild`: The server of which to get the splash for. Can be it's ID or Name. Defaults to the current server.
-        """
-
-        if not guild:
-            guild = ctx.guild
-
-        if not guild.splash:
-            raise exceptions.ArgumentError(f'The server `{guild}` does not have an splash.')
-
-        embed = discord.Embed(colour=ctx.colour, title=f"{guild.name}'s splash")
-        embed.description = f'[PNG]({guild.splash_url_as(format="png")}) | [JPEG]({guild.splash_url_as(format="jpeg")}) | [WEBP]({guild.splash_url_as(format="webp")})'
-        embed.set_image(url=str(guild.splash_url_as(format='png')))
-
-        return await ctx.send(embed=embed)
+        await ctx.send(link)
 
     @commands.command(name='server', aliases=['serverinfo'])
-    async def server(self, ctx: context.Context, *, guild: guild_converter.Guild = None):
+    async def server(self, ctx: context.Context, *, guild: guild_converter.Guild = None) -> None:
         """
         Display information about a server.
 
@@ -264,7 +200,7 @@ class Information(commands.Cog):
             else:
                 features.append(f'<:cross:739315361811267594> {description}')
 
-        embed = discord.Embed(colour=ctx.colour, title=f"{guild.name}'s information.")
+        embed = discord.Embed(colour=ctx.colour, title=f'`{guild.name}`\'s information.')
         embed.description = f'`Owner:` {guild.owner}\n' \
                             f'`Created on:` {self.bot.utils.format_datetime(datetime=guild.created_at)}\n' \
                             f'`Created:` {self.bot.utils.format_difference(datetime=guild.created_at)} ago\n' \
@@ -288,10 +224,36 @@ class Information(commands.Cog):
         embed.add_field(name='Features:', value='\n'.join(features[0:8]))
         embed.add_field(name='\u200b', value='\n'.join(features[8:16]))
         embed.set_footer(text=f'ID: {guild.id} | Owner ID: {guild.owner.id}')
-        return await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='role', aliases=['roleinfo'])
+    async def role(self, ctx: context.Context, *, role: discord.Role = None) -> None:
+        """
+        Displays information about a role.
+
+        `role`: The role to display information of. Can be its @Mention, ID or Name.
+        """
+
+        if not role:
+            if not ctx.guild:
+                raise exceptions.GeneralError('You must be in a guild to use this command without passing the `role` argument.')
+            role = ctx.author.top_role
+
+        embed = discord.Embed(colour=ctx.colour, title=f'Information about the role `{role}`')
+        embed.set_footer(text=f'ID: {role.id}')
+        embed.description = f'`Name:` {role.name}\n' \
+                            f'`Hoisted:` {role.hoist}\n' \
+                            f'`Position (from bottom):` {role.position}\n' \
+                            f'`Managed:` {role.managed}\n' \
+                            f'`Mentionable:` {role.mentionable}\n' \
+                            f'`Colour:` {str(role.colour).upper()}\n' \
+                            f'`Created at:` {self.bot.utils.format_datetime(datetime=role.created_at)}\n' \
+                            f'`Members with this role:` {len(role.members)}'
+
+        await ctx.send(embed=embed)
 
     @commands.command(name='channels')
-    async def channels(self, ctx: context.Context, *, guild: guild_converter.Guild = None):
+    async def channels(self, ctx: context.Context, *, guild: guild_converter.Guild = None) -> None:
         """
         Displays a list of a servers channels.
 
@@ -315,62 +277,84 @@ class Information(commands.Cog):
             for channel in category.channels:
                 entries.append(f'{space}{await converters.ChannelEmojiConverter().convert(ctx=ctx, channel=channel)}{channel}')
 
-        return await ctx.paginate_embed(entries=entries, per_page=30, title='List of channels, categories and voice channels.')
+        await ctx.paginate_embed(entries=entries, per_page=30, title=f'`{guild.name}`\'s channels.')
 
-    @commands.command(name='avatar', aliases=['avy'])
-    async def avatar(self, ctx: context.Context, *, user: converters.UserConverter = None):
+    @commands.command(name='icon')
+    async def icon(self, ctx: context.Context, *, guild: guild_converter.Guild = None) -> None:
         """
-        Display a user's avatar.
+        Displays a servers icon.
 
-        `user`: The user of which to get the avatar for. Can be their ID, Username, Nickname or Mention. Defaults to you.
-        """
-
-        if not user:
-            user = ctx.author
-
-        embed = discord.Embed(colour=ctx.colour, title=f"{user.name}'s avatar")
-        embed.description = f'[PNG]({user.avatar_url_as(format="png")}) | [JPEG]({user.avatar_url_as(format="jpeg")}) | [WEBP]({user.avatar_url_as(format="webp")})'
-        embed.set_image(url=str(user.avatar_url_as(format='png')))
-
-        if user.is_avatar_animated():
-            embed.description += f' | [GIF]({user.avatar_url_as(format="gif")})'
-            embed.set_image(url=str(user.avatar_url_as(format='gif')))
-
-        return await ctx.send(embed=embed)
-
-    @commands.command(name='user', aliases=['userinfo'])
-    async def user(self, ctx: context.Context, *, user: converters.UserConverter = None):
-        """
-        Displays a user's basic account information.
-
-        `user`: The user of which to get information for. Can be their ID or Username. Defaults to you.
+        `guild`: The server of which to get the icon for. Can be it's ID or Name. Defaults to the current server.
         """
 
-        if not user:
-            user = await self.bot.fetch_user(ctx.author.id)
+        if not guild:
+            guild = ctx.guild
 
-        embed = discord.Embed(colour=ctx.colour, title=f'{user}\'s information:')
-        embed.description = f'`Discord name:` {user}\n' \
-                            f'`Created on:` {self.bot.utils.format_datetime(datetime=user.created_at)}\n' \
-                            f'`Created:` {self.bot.utils.format_difference(datetime=user.created_at)} ago\n' \
-                            f'`Badges:` {self.bot.utils.badges(person=user)}\n' \
-                            f'`Bot:` {str(user.bot).replace("True", "Yes").replace("False", "No")}'
-        embed.set_thumbnail(url=str(user.avatar_url_as(format='gif' if user.is_avatar_animated() is True else 'png')))
-        embed.set_footer(text=f'ID: {user.id}')
-        return await ctx.send(embed=embed)
+        if not guild.icon:
+            raise exceptions.ArgumentError(f'The server `{guild.name}` does not have an icon.')
+
+        embed = discord.Embed(colour=ctx.colour, title=f'`{guild.name}`\'s icon')
+        embed.description = f'[PNG]({guild.icon_url_as(format="png")}) | [JPEG]({guild.icon_url_as(format="jpeg")}) | [WEBP]({guild.icon_url_as(format="webp")})'
+        embed.set_image(url=str(guild.icon_url_as(format='png')))
+
+        if guild.is_icon_animated():
+            embed.description += f' | [GIF]({guild.icon_url_as(format="gif")})'
+            embed.set_image(url=str(guild.icon_url_as(size=1024, format='gif')))
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name='banner')
+    async def banner(self, ctx: context.Context, *, guild: guild_converter.Guild = None) -> None:
+        """
+        Displays a servers banner.
+
+        `guild`: The server of which to get the banner for. Can be it's ID or Name. Defaults to the current server.
+        """
+
+        if not guild:
+            guild = ctx.guild
+
+        if not guild.banner:
+            raise exceptions.ArgumentError(f'The server `{guild.name}` does not have a banner.')
+
+        embed = discord.Embed(colour=ctx.colour, title=f'`{guild.name}`\'s banner')
+        embed.description = f'[PNG]({guild.banner_url_as(format="png")}) | [JPEG]({guild.banner_url_as(format="jpeg")}) | [WEBP]({guild.banner_url_as(format="webp")})'
+        embed.set_image(url=str(guild.banner_url_as(format='png')))
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name='splash')
+    async def splash(self, ctx: context.Context, *, guild: guild_converter.Guild = None) -> None:
+        """
+        Displays a servers splash.
+
+        `guild`: The server of which to get the splash for. Can be it's ID or Name. Defaults to the current server.
+        """
+
+        if not guild:
+            guild = ctx.guild
+
+        if not guild.splash:
+            raise exceptions.ArgumentError(f'The server `{guild.name}` does not have an splash.')
+
+        embed = discord.Embed(colour=ctx.colour, title=f'`{guild.name}`\'s splash')
+        embed.description = f'[PNG]({guild.splash_url_as(format="png")}) | [JPEG]({guild.splash_url_as(format="jpeg")}) | [WEBP]({guild.splash_url_as(format="webp")})'
+        embed.set_image(url=str(guild.splash_url_as(format='png')))
+
+        await ctx.send(embed=embed)
 
     @commands.command(name='member', aliases=['memberinfo'])
-    async def member(self, ctx: context.Context, *, member: discord.Member = None):
+    async def member(self, ctx: context.Context, *, member: discord.Member = None) -> None:
         """
         Displays a member's account information.
 
-        `member`: The member of which to get information for. Can be their ID, Username, Nickname or Mention. Defaults to you.
+        `member`: The member of which to get information for. Can be their ID, Username, Nickname or @Mention. Defaults to you.
         """
 
         if member is None:
             member = ctx.author
-        
-        embed = discord.Embed(colour=self.bot.utils.colours[member.status], title=f'{member}\'s information.')
+
+        embed = discord.Embed(colour=self.bot.utils.colours[member.status], title=f'`{member}`\'s information.')
         embed.description = f'`Discord Name:` {member} {"<:owner:738961071729278987>" if member.id == member.guild.owner.id else ""}\n' \
                             f'`Created on:` {self.bot.utils.format_datetime(datetime=member.created_at)}\n' \
                             f'`Created:` {self.bot.utils.format_difference(datetime=member.created_at)} ago\n' \
@@ -390,7 +374,49 @@ class Information(commands.Cog):
 
         embed.set_thumbnail(url=str(member.avatar_url_as(format='gif' if member.is_avatar_animated() is True else 'png')))
         embed.set_footer(text=f'ID: {member.id}')
-        return await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='user', aliases=['userinfo'])
+    async def user(self, ctx: context.Context, *, user: converters.UserConverter = None) -> None:
+        """
+        Displays a user's account information.
+
+        `user`: The user of which to get information for. Can be their ID or Username. Defaults to you.
+        """
+
+        if not user:
+            user = await self.bot.fetch_user(ctx.author.id)
+
+        embed = discord.Embed(colour=ctx.colour, title=f'`{user}`\'s information:')
+        embed.description = f'`Discord name:` {user}\n' \
+                            f'`Created on:` {self.bot.utils.format_datetime(datetime=user.created_at)}\n' \
+                            f'`Created:` {self.bot.utils.format_difference(datetime=user.created_at)} ago\n' \
+                            f'`Badges:` {self.bot.utils.badges(person=user)}\n' \
+                            f'`Bot:` {str(user.bot).replace("True", "Yes").replace("False", "No")}'
+        embed.set_thumbnail(url=str(user.avatar_url_as(format='gif' if user.is_avatar_animated() is True else 'png')))
+        embed.set_footer(text=f'ID: {user.id}')
+        await ctx.send(embed=embed)
+
+    @commands.command(name='avatar', aliases=['avy'])
+    async def avatar(self, ctx: context.Context, *, user: converters.UserConverter = None) -> None:
+        """
+        Display a user's avatar.
+
+        `user`: The user of which to get the avatar for. Can be their ID, Username, Nickname or @Mention. Defaults to you.
+        """
+
+        if not user:
+            user = ctx.author
+
+        embed = discord.Embed(colour=ctx.colour, title=f'`{user}`\'s avatar:')
+        embed.description = f'[PNG]({user.avatar_url_as(format="png")}) | [JPEG]({user.avatar_url_as(format="jpeg")}) | [WEBP]({user.avatar_url_as(format="webp")})'
+        embed.set_image(url=str(user.avatar_url_as(format='png')))
+
+        if user.is_avatar_animated():
+            embed.description += f' | [GIF]({user.avatar_url_as(format="gif")})'
+            embed.set_image(url=str(user.avatar_url_as(format='gif')))
+
+        await ctx.send(embed=embed)
 
     @commands.command(name='rolecounts', aliases=['rcs', 'roles'])
     async def role_counts(self, ctx: context.Context) -> None:
