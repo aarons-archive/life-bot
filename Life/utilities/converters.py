77fc81d53413f162edc1e023b43882f2eee19685
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-05-02 04:16:57 +0100

    improve wrong timezone message to ease confusion

--- Life/utilities/converters.py
+++ Life/utilities/converters.py
@@ -49,9 +49,7 @@ class TimezoneConverter(commands.Converter, ABC):
     async def convert(self, ctx: context.Context, argument: str) -> Timezone:
 
         if argument not in pendulum.timezones:
-            msg = '\n'.join(f'`{index + 1}.` {match[0]}' for index, match in enumerate(
-                    rapidfuzz.process.extract(query=argument, choices=pendulum.timezones, processor=lambda s: s)
-            ))
+            msg = '\n'.join(f'- `{match[0]}`' for index, match in rapidfuzz.process.extract(query=argument, choices=pendulum.timezones, processor=lambda s: s))
             raise exceptions.ArgumentError(f'That was not a recognised timezone. Maybe you meant one of these?\n{msg}')
 
         return pendulum.timezone(argument)
