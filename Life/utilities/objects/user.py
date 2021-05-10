d7ed2f4908a28da54ff3061ac7725ec94432e7ae
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-05-10 19:05:59 +0100

    fix incorrect next_birthday attr and typehints

--- Life/utilities/objects/user.py
+++ Life/utilities/objects/user.py
@@ -99,7 +99,7 @@ class UserConfig:
         return self._timezone_private
 
     @property
-    def birthday(self) -> Optional[pendulum.timezone]:
+    def birthday(self) -> Optional[pendulum.datetime]:
         return self._birthday
 
     @property
@@ -143,13 +143,9 @@ class UserConfig:
             return None
 
         now = pendulum.now(tz='UTC')
+        year = now.year if now < self.birthday.add(years=self.age) else self.birthday.year + self.age + 1
 
-        return now.replace(
-                year=now.year + 1 if now > self.birthday.add(years=self.age) else now.year,
-                month=self.birthday.month,
-                day=self.birthday.day,
-                hour=12, minute=0, second=0, microsecond=0
-        )
+        return now.replace(year=year, month=self.birthday.month, day=self.birthday.day, hour=0, minute=0, second=0, microsecond=0)
 
     @property
     def time(self) -> Optional[pendulum.datetime]:
