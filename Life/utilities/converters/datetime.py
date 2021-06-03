746425b32499a10e623d32116e28dcaa5f1458fc
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-06-03 21:09:46 +0100

    add dict types to DatetimeConverter

--- Life/utilities/converters/datetime.py
+++ Life/utilities/converters/datetime.py
@@ -8,7 +8,8 @@
 #  PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 #
 #  You should have received a copy of the GNU Affero General Public License along with Life. If not, see https://www.gnu.org/licenses/.
-#
+
+from typing import Union
 
 import dateparser.search
 import pendulum
@@ -20,7 +21,7 @@ from utilities import context, exceptions
 
 class DatetimeConverter(commands.Converter):
 
-    async def convert(self, ctx: context.Context, argument: str) -> dict:
+    async def convert(self, ctx: context.Context, argument: str) -> dict[str, Union[str, pendulum.DateTime]]:
 
         searches = dateparser.search.search_dates(argument, languages=['en'], settings=config.DATEPARSER_SETTINGS)
         if not searches:
@@ -29,8 +30,7 @@ class DatetimeConverter(commands.Converter):
         data = {'argument': argument, 'found': {}}
 
         for datetime_phrase, datetime in searches:
-            datetime = pendulum.instance(dt=datetime, tz='UTC')
-            data['found'][datetime_phrase] = datetime
+            data['found'][datetime_phrase] = pendulum.instance(dt=datetime, tz='UTC')
 
         if not data['found']:
             raise exceptions.ArgumentError('I was able to find a time and/or date within your query, however it seems to be in the past.')
