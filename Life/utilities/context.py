7cf41956dc81b67eb659a9a8d9d32be73a1ae402
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-06-03 18:55:01 +0100

    cleanup

--- Life/utilities/context.py
+++ Life/utilities/context.py
@@ -8,7 +8,6 @@
 #  PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 #
 #  You should have received a copy of the GNU Affero General Public License along with Life. If not, see https://www.gnu.org/licenses/.
-#
 
 from __future__ import annotations
 
