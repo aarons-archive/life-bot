8d9d57b426b28e913a74e6769979228d25734be6
Aaron Hennessey <axelancerr@gmail.com>
2021-06-19 02:56:03 +0100

    add new aliases and license to emoji file

--- Life/emoji.py
+++ Life/emoji.py
@@ -1,3 +1,25 @@
+"""
+Copyright (c) 2020-present Axelancerr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 CHANNELS = {
     'TEXT':                '<:text:855190519717822515>',
     'TEXT_LOCKED':         '<:text_locked:855190519902765117>',
@@ -63,6 +85,11 @@ DOWN = '<:down:855193257364815883>'
 
 NEXT = '<:next:855193257419603970>'
 PREVIOUS = '<:previous:855193257633120286>'
+
+FIRST = PREVIOUS
+BACKWARD = ARROW_LEFT
+FORWARD = ARROW_RIGHT
+LAST = NEXT
 
 REPEAT = '<:repeat:855193257885696023>'
 REPEAT_CURRENT = '<:repeat_current:855193257825534043>'
