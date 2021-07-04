c5ee971019c019c73d01312761df011e7bef3720
Aaron Hennessey <axelancerr@gmail.com>
2021-07-04 18:23:41 +0100

    add queue commands
    - Add all queue related commands.
    - Makes `l-lyrics player` use the track author as well.

--- Life/utilities/checks/queue_not_empty.py
+++ Life/utilities/checks/queue_not_empty.py
@@ -20,12 +20,21 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from utilities.checks.global_check import global_check
-from utilities.checks.has_any_permission import has_any_permissions
-from utilities.checks.has_voice_client import has_voice_client
-from utilities.checks.is_author_connected import is_author_connected
-from utilities.checks.is_guild_owner import is_guild_owner
-from utilities.checks.is_track_requester import is_track_requester
-from utilities.checks.is_track_seekable import is_track_seekable
-from utilities.checks.is_voice_client_playing import is_voice_client_playing
-from utilities.checks.queue_not_empty import queue_not_empty
+from typing import Literal
+
+from discord.ext import commands
+
+from core import colours, emojis
+from utilities import exceptions, context
+
+
+def queue_not_empty():
+
+    async def predicate(ctx: context.Context) -> Literal[True]:
+
+        if not ctx.voice_client or ctx.voice_client.queue.is_empty():
+            raise exceptions.EmbedError(colour=colours.RED, emoji=emojis.CROSS, description='The queue is empty.')
+
+        return True
+
+    return commands.check(predicate)
