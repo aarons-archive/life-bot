cd3f055a260b4d48a7c1355f6434be234a3034de
Aaron Hennessey <axelancerr@gmail.com>
2021-08-06 20:18:05 +0100

    fuck off sourcery

--- Life/utilities/checks/is_author_connected.py
+++ Life/utilities/checks/is_author_connected.py
@@ -13,10 +13,8 @@ def is_author_connected(same_channel: bool):
         author_channel = ctx.author.voice and ctx.author.voice.channel
         voice_client_channel = ctx.voice_client and ctx.voice_client.voice_channel
 
-        if same_channel is True and voice_client_channel is not None:
-
-            if author_channel != voice_client_channel:
-                raise exceptions.EmbedError(colour=colours.RED, emoji=emojis.CROSS, description=f"You must be connected to {voice_client_channel.mention} to use this command.")
+        if same_channel is True and voice_client_channel is not None and author_channel != voice_client_channel:
+            raise exceptions.EmbedError(colour=colours.RED, emoji=emojis.CROSS, description=f"You must be connected to {voice_client_channel.mention} to use this command.")
 
         if not author_channel:
             raise exceptions.EmbedError(colour=colours.RED, emoji=emojis.CROSS, description=f"You must be connected to a voice channel to use this command.")
