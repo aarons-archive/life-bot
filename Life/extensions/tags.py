57358bbb3c56652741e699caefb81a3e5dab4726
Aaron Hennessey <axelancerr@gmail.com>
2021-08-07 04:45:19 +0100

    switch tags extensions to "

--- Life/extensions/tags.py
+++ Life/extensions/tags.py
@@ -13,31 +13,31 @@ class Tags(commands.Cog):
     def __init__(self, bot: Life) -> None:
         self.bot = bot
 
-    @commands.group(name='tag', aliases=['tags'], invoke_without_command=True)
+    @commands.group(name="tag", aliases=["tags"], invoke_without_command=True)
     async def tag(self, ctx: context.Context, *, name: converters.TagNameConverter) -> None:
         """
-        Get a tag using its name or alias.
+        Gets a tag
 
-        `name`: The name or alias of the tag that you want to find.
+        **name**: The name or alias of the tag that you want to get.
         """
         name = str(name)
 
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tags := guild_config.get_tags_matching(name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
         tag = tags[0]
 
         if tag.name != name:
-            msg = f'Maybe you meant one of these?\n{values.NL.join(f"- `{tag.name}`" for tag in tags[0:])}' if len(tags) > 1 else ''
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`. {msg}')
+            msg = f"Maybe you meant one of these?\n{values.NL.join(f'- `{tag.name}`' for tag in tags[0:])}" if len(tags) > 1 else ""
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`. {msg}")
 
         if tag.alias:
             tag = guild_config.get_tag(tag_id=tag.alias)
 
         await ctx.reply(tag.content)
 
-    @tag.command(name='raw')
+    @tag.command(name="raw")
     async def tag_raw(self, ctx: context.Context, *, name: converters.TagNameConverter) -> None:
         """
         Get a tags raw content.
@@ -49,19 +49,19 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tags := guild_config.get_tags_matching(name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
         tag = tags[0]
 
         if tag.name != name:
-            msg = f'Maybe you meant one of these?\n{values.NL.join(f"- `{tag.name}`" for tag in tags[0:])}' if len(tags) > 1 else ''
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`. {msg}')
+            msg = f"Maybe you meant one of these?\n{values.NL.join(f'- `{tag.name}`' for tag in tags[0:])}" if len(tags) > 1 else ""
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`. {msg}")
 
         if tag.alias:
             tag = guild_config.get_tag(tag_id=tag.alias)
 
         await ctx.reply(discord.utils.escape_markdown(tag.content))
 
-    @tag.command(name='create', aliases=['make'])
+    @tag.command(name="create", aliases=["make"])
     async def tag_create(self, ctx: context.Context, name: converters.TagNameConverter, *, content: converters.TagContentConverter) -> None:
         """
         Create a tag.
@@ -75,12 +75,12 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if tag_check := guild_config.get_tag(tag_name=name):
-            raise exceptions.ArgumentError(f'There is already a tag with the name `{tag_check.name}`.')
+            raise exceptions.ArgumentError(f"There is already a tag with the name `{tag_check.name}`.")
 
         tag = await guild_config.create_tag(user_id=ctx.author.id, name=name, content=content, jump_url=ctx.message.jump_url)
-        await ctx.reply(f'Created tag with name `{tag.name}`.')
+        await ctx.reply(f"Created tag with name `{tag.name}`.")
 
-    @tag.command(name='alias')
+    @tag.command(name="alias")
     async def tag_alias(self, ctx: context.Context, alias: converters.TagNameConverter, original: converters.TagNameConverter) -> None:
         """
         Alias a new tag to a pre-existing tag.
@@ -94,18 +94,18 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if tag_check := guild_config.get_tag(tag_name=alias):
-            raise exceptions.ArgumentError(f'There is already a tag with the name `{tag_check.name}`.')
+            raise exceptions.ArgumentError(f"There is already a tag with the name `{tag_check.name}`.")
 
         if not (original_tag := guild_config.get_tag(tag_name=original)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{original}` to alias too.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{original}` to alias too.")
 
         if original_tag.alias is not None:
             original_tag = guild_config.get_tag(tag_id=original_tag.alias)
 
         tag = await guild_config.create_tag_alias(user_id=ctx.author.id, name=alias, original=original_tag.id, jump_url=ctx.message.jump_url)
-        await ctx.reply(f'Tag alias from `{tag.name}` to `{original}` was created.')
+        await ctx.reply(f"Tag alias from `{tag.name}` to `{original}` was created.")
 
-    @tag.command(name='claim')
+    @tag.command(name="claim")
     async def tag_claim(self, ctx: context.Context, *, name: converters.TagNameConverter) -> None:
         """
         Claim a tag if its owner has left the server.
@@ -117,15 +117,15 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tag := guild_config.get_tag(tag_name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
 
         if ctx.guild.get_member(tag.user_id):
-            raise exceptions.ArgumentError('The owner of that tag is still in this server.')
+            raise exceptions.ArgumentError("The owner of that tag is still in this server.")
 
         await tag.change_owner(ctx.author.id)
-        await ctx.reply('Transferred tag to you.')
+        await ctx.reply("Transferred tag to you.")
 
-    @tag.command(name='transfer')
+    @tag.command(name="transfer")
     async def tag_transfer(self, ctx: context.Context, name: converters.TagNameConverter, *, member: discord.Member) -> None:
         """
         Transfer a tag to another member.
@@ -138,22 +138,22 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if member.bot:
-            raise exceptions.ArgumentError('You can not transfer tags to bots.')
+            raise exceptions.ArgumentError("You can not transfer tags to bots.")
 
         if not (tag := guild_config.get_tag(tag_name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
         if tag.user_id != ctx.author.id:
-            raise exceptions.ArgumentError('You do not own that tag.')
+            raise exceptions.ArgumentError("You do not own that tag.")
         if tag.user_id == member.id:
-            raise exceptions.ArgumentError('You can not transfer tags to yourself.')
+            raise exceptions.ArgumentError("You can not transfer tags to yourself.")
 
         await tag.change_owner(user_id=member.id)
-        await ctx.reply(f'Transferred tag from `{ctx.author}` to `{ctx.guild.get_member(tag.user_id)}`.')
+        await ctx.reply(f"Transferred tag from `{ctx.author}` to `{ctx.guild.get_member(tag.user_id)}`.")
 
-    @tag.command(name='edit')
+    @tag.command(name="edit")
     async def tag_edit(self, ctx: context.Context, name: converters.TagNameConverter, *, content: converters.TagContentConverter) -> None:
         """
-        Edit a tag's content.
+        Edit a tag"s content.
 
         `name`: The name of the tag to edit the content of.
         `content:` The content to edit the tag with.
@@ -164,14 +164,14 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tag := guild_config.get_tag(tag_name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
         if tag.user_id != ctx.author.id:
-            raise exceptions.ArgumentError('You do not own that tag.')
+            raise exceptions.ArgumentError("You do not own that tag.")
 
         await tag.change_content(content)
-        await ctx.reply(f'Edited content of tag with name `{name}`.')
+        await ctx.reply(f"Edited content of tag with name `{name}`.")
 
-    @tag.command(name='delete', aliases=['remove'])
+    @tag.command(name="delete", aliases=["remove"])
     async def tag_delete(self, ctx: context.Context, *, name: converters.TagNameConverter) -> None:
         """
         Delete a tag.
@@ -183,14 +183,14 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tag := guild_config.get_tag(tag_name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
         if tag.user_id != ctx.author.id:
-            raise exceptions.ArgumentError('You do not own that tag.')
+            raise exceptions.ArgumentError("You do not own that tag.")
 
         await tag.delete()
-        await ctx.reply(f'Deleted tag with name `{name}`.')
+        await ctx.reply(f"Deleted tag with name `{name}`.")
 
-    @tag.command(name='search')
+    @tag.command(name="search")
     async def tag_search(self, ctx: context.Context, *, name: converters.TagNameConverter) -> None:
         """
         Display a list of tags that are similar to the search.
@@ -202,15 +202,15 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tags := guild_config.get_tags_matching(name=name, limit=100)):
-            raise exceptions.ArgumentError(f'There are no tags similar to the search `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags similar to the search `{name}`.")
 
-        entries = [f'`{index + 1}.` {tag.name}' for index, tag in enumerate(tags)]
-        await ctx.paginate_embed(entries=entries, per_page=25, title=f'Tags matching: `{name}`')
+        entries = [f"`{index + 1}.` {tag.name}" for index, tag in enumerate(tags)]
+        await ctx.paginate_embed(entries=entries, per_page=25, title=f"Tags matching: `{name}`")
 
-    @tag.command(name='list')
+    @tag.command(name="list")
     async def tag_list(self, ctx: context.Context, *, member: Optional[discord.Member]) -> None:
         """
-        Get a list of yours or someone else's tags.
+        Get a list of yours or someone else"s tags.
 
         `member`: The member to get a tag list for. Can be their ID, Username, Nickname or @Mention.
         """
@@ -221,12 +221,12 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tags := guild_config.get_user_tags(member.id)):
-            raise exceptions.ArgumentError(f'`{member}` does not have any tags.')
+            raise exceptions.ArgumentError(f"`{member}` does not have any tags.")
 
-        entries = [f'`{index + 1}.` {tag.name}' for index, tag in enumerate(tags)]
-        await ctx.paginate_embed(entries=entries, per_page=25, title=f'`{member}`\'s tags:')
+        entries = [f"`{index + 1}.` {tag.name}" for index, tag in enumerate(tags)]
+        await ctx.paginate_embed(entries=entries, per_page=25, title=f"`{member}`\"s tags:")
 
-    @tag.command(name='all')
+    @tag.command(name="all")
     async def tag_all(self, ctx: context.Context) -> None:
         """
         Get a list of all tags in this server.
@@ -235,12 +235,12 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tags := guild_config.get_all_tags()):
-            raise exceptions.ArgumentError('There are no tags.')
+            raise exceptions.ArgumentError("There are no tags.")
 
-        entries = [f'`{index + 1}.` {tag.name}' for index, tag in enumerate(tags)]
-        await ctx.paginate_embed(entries=entries, per_page=25, title=f'`{ctx.guild}`\'s Tags:')
+        entries = [f"`{index + 1}.` {tag.name}" for index, tag in enumerate(tags)]
+        await ctx.paginate_embed(entries=entries, per_page=25, title=f"`{ctx.guild}`\"s Tags:")
 
-    @tag.command(name='info')
+    @tag.command(name="info")
     async def tag_info(self, ctx: context.Context, *, name: converters.TagNameConverter) -> None:
         """
         Displays information about a tag.
@@ -252,19 +252,19 @@ class Tags(commands.Cog):
         guild_config = await self.bot.guild_manager.get_or_create_config(ctx.guild.id)
 
         if not (tag := guild_config.get_tag(tag_name=name)):
-            raise exceptions.ArgumentError(f'There are no tags with the name `{name}`.')
+            raise exceptions.ArgumentError(f"There are no tags with the name `{name}`.")
 
         owner = ctx.guild.get_member(tag.user_id)
 
         embed = discord.Embed(
-                colour=colours.MAIN, title=f'{tag.name}',
-                description=f'''
-                `Owner:` {owner.mention if owner else "*Not found*"} ({tag.user_id})
+                colour=colours.MAIN, title=f"{tag.name}",
+                description=f"""
+                `Owner:` {owner.mention if owner else '*Not found*'} ({tag.user_id})
                 `Claimable:` {owner is None}
                 `Alias:` {guild_config.get_tag(tag_id=tag.alias).name if tag.alias else None}
                 `Created on:` {utils.format_datetime(tag.created_at)}
                 `Created:` {utils.format_difference(tag.created_at)} ago
-                '''
+                """
         )
         await ctx.reply(embed=embed)
 
