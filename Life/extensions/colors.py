eea6f32856c035c42a577aba4ce21cbb5f539824
MrArkon <71749203+MrArkon@users.noreply.github.com>
2021-08-11 12:02:30 +0530

    Added all required imports for randomcolor command

--- Life/extensions/colors.py
+++ Life/extensions/colors.py
@@ -1,7 +1,13 @@
+import randomcolor
+import webcolors
+from io import BytesIO
+from PIL import Image
+
 import discord
 from discord.ext import commands
 
 from core.bot import Life
+from utilities import decorators
 
 def setup(bot: Life) -> None:
   bot.add_cog(Colors(bot=bot))
