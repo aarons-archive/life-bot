7a0e8bb7a3c55acaef0d1f70169a4de85d5b6392
Aaron Hennessey <axelancerr@gmail.com>
2021-09-09 22:45:10 +0100

    update values with beta bot invite + comment

--- bot/core/values.py
+++ bot/core/values.py
@@ -8,9 +8,9 @@ import discord
 from core import config
 
 
+# Common values
 ZWSP = "\u200b"
 NL = "\n"
-
 PERMISSIONS = discord.Permissions(
     read_messages=True,
     send_messages=True,
@@ -21,8 +21,29 @@ PERMISSIONS = discord.Permissions(
     external_emojis=True,
 )
 
-INVITE_LINK = discord.utils.oauth_url(client_id=config.BOT_ID, permissions=PERMISSIONS, scopes=["bot"])
-INVITE_LINK_NO_PERMISSIONS = discord.utils.oauth_url(client_id=config.BOT_ID)
 
+# Discord links
+INVITE_LINK = discord.utils.oauth_url(
+    client_id=config.BOT_ID,
+    permissions=PERMISSIONS,
+    scopes=["bot", "applications.commands"]
+)
+INVITE_LINK_NO_PERMISSIONS = discord.utils.oauth_url(
+    client_id=config.BOT_ID,
+    scopes=["bot"]
+)
+
+BETA_INVITE_LINK = discord.utils.oauth_url(
+    client_id=config.BETA_BOT_ID,
+    permissions=PERMISSIONS,
+    scopes=["bot", "applications.commands"]
+)
+BETA_INVITE_LINK_NO_PERMISSIONS = discord.utils.oauth_url(
+    client_id=config.BOT_ID,
+    scopes=["bot"]
+)
+
+
+# External links
 SUPPORT_LINK = "https://discord.gg/w9f6NkQbde"
 GITHUB_LINK = "https://github.com/Axelancerr/Life-bot"
